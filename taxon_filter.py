63766915e193b26256fa29868ddea1bcf29f0078
Danny Park <dpark@broadinstitute.org>
2014-09-26 21:43:33 -0400

    clean up imports, rename one file, add a new script (empty)

---
+++ taxon_filter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+'''This script contains a number of utilities for filtering NGS reads based
+on membership or non-membership in a species / genus / taxonomic grouping.
+
+Requires python >= 2.7 and BioPython.  On the Broad cluster, it is known
+to work with the Python-2.7 and Python-3.4 dotkits.'''
+
+__author__ = "dpark@broadinstitute.org"
+__version__ = "PLACEHOLDER"
+__date__ = "PLACEHOLDER"
+__commands__ = []
+
+import argparse, logging
+import util.cmd, util.file, util.vcf, util.misc
+
+log = logging.getLogger(__name__)
+global_tool_paths = {}
+
+
+
+if __name__ == '__main__':
+	util.cmd.main_argparse(__commands__, __doc__)
