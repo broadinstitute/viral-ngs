66145404512b9dcf8309629d364346e50885746c
Danny Park <dpark@broadinstitute.org>
2014-09-29 10:32:36 -0400

    add ncbi script, tweak taxon_filter template

--- ncbi.py
+++ ncbi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-'''This script contains a number of utilities for filtering NGS reads based
-on membership or non-membership in a species / genus / taxonomic grouping.
+'''This script contains a number of utilities for submitting our analyses
+to NCBI's Genbank and SRA databases.
 
 Requires python >= 2.7 and BioPython.  On the Broad cluster, it is known
 to work with the Python-2.7 and Python-3.4 dotkits.'''
