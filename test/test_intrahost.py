f1f56bdc2ddc0cf29d3f027c35d3771f95635743
Danny Park <dpark@broadinstitute.org>
2014-10-01 16:06:12 -0400

    add empty test files

---
+++ test/test_intrahost.py
@@ -0,0 +1,8 @@
+# Unit tests for intrahost.py
+
+__author__ = "PLACEHOLDER"
+
+import intrahost
+import unittest
+
+pass
---
+++ test/test_ncbi.py
@@ -0,0 +1,8 @@
+# Unit tests for ncbi.py
+
+__author__ = "PLACEHOLDER"
+
+import ncbi
+import unittest
+
+pass
