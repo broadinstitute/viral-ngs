671da299e0c4f051e73ec6e9d856ea7ec12c864a
Irwin Jungreis <iljungr@csail.mit.edu>
2014-10-11 17:15:23 -0400

    First attempt at TestFilterLastal.

--- test/test_taxon_filter.py
+++ test/test_taxon_filter.py
@@ -1,9 +1,10 @@
 # Unit tests for taxon_filter.py
 
-__author__ = "PLACEHOLDER"
+__author__ = "dpark@broadinstitute.org, irwin@broadinstitute.org"
 
-import taxon_filter
-import unittest
+import taxon_filter, util
+from test_consensus import set_tmpDir, destroy_tmpDir
+import unittest, os
 
 class TestCommandHelp(unittest.TestCase):
 	def test_help_parser_for_each_command(self):
@@ -15,3 +16,30 @@ class TestTaxonFilter(unittest.TestCase):
 	def testNothingAtAll(self):
 		'''here we test nothing at all and this should pass'''
 		pass
+
+class TestFilterLastal(unittest.TestCase):
+	def setUp(self):
+		set_tmpDir('TestFilterLastal')
+	def tearDown(self):
+		destroy_tmpDir()
+	def test_filter_lastal(self) :
+		filterLastalInput = """@fakeRead
+		AGTACATGCAGAGCAAGGACTGATACAATATCCAACAGCTTGGCAATCAGTAGGACACATGATGGTGA
+		+
+		CCCFFFFFHHHHHJJJJJJJJJJJJJJJHIIIIJJJJHIJIIJJIJJFHGIIJJGHHHBDFDDDDDDD
+		"""
+		filterLastalExpected = """@fakeRead
+		AGTACATGCAGAGCAAGGACTGATACAATATCCAACAGCTTGGCAATCAGTAGGACACATGATGGTGA
+		+fakeRead
+		CCCFFFFFHHHHHJJJJJJJJJJJJJJJHIIIIJJJJHIJIIJJIJJFHGIIJJGHHHBDFDDDDDDD
+		"""
+		refDbs = os.path.join(os.path.dirname(__file__), 'input', 'ebola')
+		inFastq = util.file.mkstempfname()
+		outFastq = util.file.mkstempfname()
+		open(inFastq, 'w').write(filterLastalInput)
+		args = taxon_filter.parser_filter_lastal().parse_args([inFastq, refDbs, outFastq])
+		taxon_filter.main_filter_lastal(args)
+		self.assertEqual(open(outFastq + '.fastq').read(), filterLastalExpected)
+
+if __name__ == '__main__':
+    unittest.main()
