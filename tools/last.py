84b0f08eeb74822826d6cf170661145959d98c42
Irwin Jungreis <iljungr@csail.mit.edu>
2014-10-11 13:42:58 -0400

    First pass implementation of main_filter_lastal.

--- tools/last.py
+++ tools/last.py
@@ -1,10 +1,23 @@
-# /idi/sabeti-scratch/kandersen/bin/last/lastal
+"Tools in the 'last' suite."
 
+import tools, os
 
-import tools
+lastBroadUnixPath = '/idi/sabeti-scratch/kandersen/bin/last'
 
-class BroadUnix(tools.PrexistingUnixCommand):
-	def __init__(self, path='/idi/sabeti-scratch/kandersen/bin/last/lastal'):
-		super(BroadUnix, self).__init__(path=path)
-	def verify_install(self):
-		super(BroadUnix, self).verify_install()
+class LastTools(tools.Tool) :
+	"""Abstract base class for tools in the 'last' suite.
+	   Subclasses must define a class member subtoolName."""
+	def __init__(self, install_methods = None):
+		if install_methods == None:
+			path = os.path.join(lastBroadUnixPath, self.subtoolName)
+			install_methods = [tools.PrexistingUnixCommand(path)]
+		super(LastTools, self).__init__(install_methods = install_methods)
+
+class Lastal(LastTools) :
+	subtoolName = 'lastal'
+
+class MafSort(LastTools) :
+	subtoolName = 'maf-sort'
+
+class MafConvert(LastTools) :
+	subtoolName = 'maf-convert'
--- tools/prinseq.py
+++ tools/prinseq.py
@@ -1,1 +1,11 @@
-# /idi/sabeti-scratch/kandersen/bin/prinseq/prinseq-lite.pl
+"tools.Tool for prinseq."
+
+import tools
+
+prinseqBroadUnixPath = '/idi/sabeti-scratch/kandersen/bin/prinseq/prinseq-lite.pl'
+
+class PrinseqTool(tools.Tool) :
+	def __init__(self, install_methods = None):
+		if install_methods == None:
+			install_methods = [tools.PrexistingUnixCommand(prinseqBroadUnixPath)]
+		super(PrinseqTool, self).__init__(install_methods = install_methods)
