3948e1ce27f82e2bf2562420452b973a8d1bcac8
Irwin Jungreis <iljungr@csail.mit.edu>
2014-10-22 16:56:28 -0400

    DownloadPackage now uses relative path, defaults to build dir, has post_download_command, and handles .bz2. Removed DownloadScript.

--- tools/last.py
+++ tools/last.py
@@ -1,32 +1,29 @@
 "Tools in the 'last' suite."
 
 import tools, util.file
-import os, tempfile
+import os
 
 #lastBroadUnixPath = '/idi/sabeti-scratch/kandersen/bin/last'
 
 class LastTools(tools.Tool) :
 	"""Abstract base class for tools in the 'last' suite.
 	   Subclasses must define class members subtoolName and subtoolNameOnBroad."""
-	def __init__(self, install_methods = None):
-		if install_methods == None:
+	def __init__(self, install_methods = None) :
+		if install_methods == None :
 			install_methods = []
+			install_methods.append(DownloadAndBuildLast(self.subtoolName))
 			#Version of last on broad is old, database-incompatible with newer one,
 			#   so don't use it and always load the newer version
 			#path = os.path.join(lastBroadUnixPath, self.subtoolNameOnBroad)
 			#install_methods.append(tools.PrexistingUnixCommand(path))
-			install_methods.append(DownloadAndBuildLast(self.subtoolName))
 		tools.Tool.__init__(self, install_methods = install_methods)
 
 class DownloadAndBuildLast(tools.DownloadPackage) :
 	lastWithVersion = 'last-490'
 	def __init__(self, subtoolName) :
 		url = 'http://last.cbrc.jp/{}.zip'.format(self.lastWithVersion)
-		buildDir = util.file.get_build_path()
-		targetpath = os.path.join(buildDir, self.lastWithVersion, 'bin', subtoolName)
-		destination_dir = buildDir
-		tools.DownloadPackage.__init__(self, url = url, targetpath = targetpath,
-									   destination_dir = destination_dir)
+		target_rel_path = os.path.join(self.lastWithVersion, 'bin', subtoolName)
+		tools.DownloadPackage.__init__(self, url, target_rel_path)
 	def post_download(self) :
 		path = os.path.join(util.file.get_build_path(), self.lastWithVersion)
 		os.system('cd {}; make; make install prefix=.'.format(path))
