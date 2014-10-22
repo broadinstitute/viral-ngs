4b65e83e5bcceacb26f6a916b349c1145dd0c900
Irwin Jungreis <iljungr@csail.mit.edu>
2014-10-22 16:56:52 -0400

    Added tools for picard and samtools.

--- tools/samtools.py
+++ tools/samtools.py
@@ -1,1 +1,16 @@
-#	'samtools':	'/idi/sabeti-data/software/samtools/samtools-0.1.19',
+import tools, util.file
+import os
+
+url = 'http://sourceforge.net/projects/samtools/files/samtools/0.1.19/samtools-0.1.19.tar.bz2'
+
+class SamtoolsTool(tools.Tool) :
+	def __init__(self, install_methods = None) :
+		if install_methods == None :
+			install_methods = []
+			install_methods.append(
+				tools.DownloadPackage(url, 'samtools-0.1.19/samtools',
+									  post_download_command = 'cd samtools-0.1.19; make'))
+			#path = '/idi/sabeti-data/software/samtools/samtools-0.1.19/samtools',
+			#install_methods.append(tools.PrexistingUnixCommand(path))
+		tools.Tool.__init__(self, install_methods = install_methods)
+
