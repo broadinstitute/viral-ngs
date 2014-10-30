0146758ccf93eabf22f5278a4fa4f036ebd1e098
Hanna Mendes Levitin <h.mendes.levitin@gmail.com>
2014-10-29 23:38:09 -0400

    more 80-complete files

--- tools/picard.py
+++ tools/picard.py
@@ -1,8 +1,9 @@
 "Tools in the 'picard' suite."
 import tools
 
-url = 'https://github.com/broadinstitute/picard/releases/download/1.123/picard-tools-1.123.zip'
-#         Note: Version 1.123 is latest as of 2014-10-22
+url = 'https://github.com/broadinstitute/picard/releases/download/1.123/' \
+        + 'picard-tools-1.123.zip'
+# Note: Version 1.123 is latest as of 2014-10-22
 picardBroadUnix = '/seq/software/picard/current/bin'
 
 class PicardTool(tools.Tool) :
@@ -12,11 +13,11 @@ class PicardTool(tools.Tool) :
         if install_methods == None :
             install_methods = []
             target_rel_path = 'picard-tools-1.123/{}'.format(self.subtoolName)
-            install_methods.append(tools.DownloadPackage(url, target_rel_path,
-                                                         require_executability=False))
+            install_methods.append( tools.DownloadPackage(url, target_rel_path,
+                require_executability=False))
             #broadUnixPath = picardBroadUnix + '/' + subtoolName
             #install_methods.append(tools.PrexistingUnixCommand(broadUnixPath,
-            #                                                  require_executability=False))
+                # require_executability=False))
         tools.Tool.__init__(self, install_methods = install_methods)
 
 class MarkDuplicatesTool(PicardTool) :
