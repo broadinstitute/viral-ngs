2f83753e6957712c89cc1e30ec8b05759972e041
Irwin Jungreis <iljungr@csail.mit.edu>
2014-11-08 14:31:38 -0500

    Created test_read_utils; moved TestPurgeUnmated there.

--- test/test_taxon_filter.py
+++ test/test_taxon_filter.py
@@ -3,7 +3,7 @@
 __author__ = "dpark@broadinstitute.org, irwin@broadinstitute.org," \
                 + "hlevitin@broadinstitute.org"
 
-import unittest, os, sys, tempfile, shutil
+import unittest, os, tempfile, shutil
 import taxon_filter, util.file, tools.last, tools.bmtagger, tools.blast
 from test import assert_equal_contents, TestCaseWithTmp
 
@@ -174,24 +174,6 @@ class TestDepleteBlastn(TestCaseWithTmp) :
         # Compare to expected
         assert_equal_contents(self, outFile,
                               os.path.join(myInputDir, 'expected.fastq'))
-
-class TestPurgeUnmated(TestCaseWithTmp) :
-    def test_purge_unmated(self) :
-        tempDir = tempfile.mkdtemp()
-        myInputDir = util.file.get_test_input_path(self)
-        inFastq1 = os.path.join(myInputDir, 'in1.fastq')
-        inFastq2 = os.path.join(myInputDir, 'in2.fastq')
-        outFastq1 = util.file.mkstempfname()
-        outFastq2 = util.file.mkstempfname()
-        parser = util.read_utils.parser_purge_unmated()
-        args = parser.parse_args([inFastq1, inFastq2, outFastq1, outFastq2])
-        util.read_utils.main_purge_unmated(args)
-
-        # Check that results match expected
-        expected1Fastq = os.path.join(myInputDir, 'expected1.fastq')
-        expected2Fastq = os.path.join(myInputDir, 'expected2.fastq')
-        assert_equal_contents(self, outFastq1, expected1Fastq)
-        assert_equal_contents(self, outFastq2, expected2Fastq)
 
 
 if __name__ == '__main__':
