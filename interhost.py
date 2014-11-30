4c45b2885a3e95d04aa2a3c4186f2b1bb2b7a6b7
Daniel Park <dpark@broadinstitute.org>
2014-11-30 15:57:15 -0500

    modify rachels calls_snps_3 code to be py2.7 friendly

--- interhost.py
+++ interhost.py
@@ -16,49 +16,46 @@ log = logging.getLogger(__name__)
 
 
 
-# rachel's call_snps_3.py follows
-def call_snps_3(inFasta):
-    REF = "EBOV_2014_G3686"
-    CHROM = "KM034562"
+# modified version of rachel's call_snps_3.py follows
+def call_snps_3(inFasta, outVcf, REF="KJ660346.2"):
     a=Bio.AlignIO.read(inFasta, "fasta")
     ref_idx = find_ref(a, REF)
-    print_header(a)
-    make_vcf(a, ref_idx, CHROM)
-def print_header(a):
-    print("##fileformat=VCFv4.1")
-    print("##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">")
-    print("##contig=<ID=\"KM034562\",length=18957>")
-    print("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t", end="")
-    for i in range(0, len(a) - 1):
-        print(a[i].id + "\t", end="")
-    print(a[len(a) - 1].id + "\n", end="")
+    with open(outVcf, 'wt') as outf:
+        outf.write(vcf_header(a))
+        for row in make_vcf(a, ref_idx, REF):
+            outf.write('\t'.join(map(str, row))+'\n')
 def find_ref(a, ref):
-    for i in range(0, len(a)):
+    for i in range(len(a)):
         if a[i].id == ref:
             return i
     return -1
+def vcf_header(a):
+    header = "##fileformat=VCFv4.1\n"
+    header += "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n"
+    header += "##contig=<ID=\"KM034562\",length=18957>\n"
+    header += '#' + '\t'.join(['CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT'] + [x.id for x in a]) + '\n'
+    return header
 def make_vcf(a, ref_idx, chrom):
     bases=set(["A", "C", "G", "T"])
-    for i in range(0,len(a[0])):
+    for i in range(len(a[0])):
         alt = []
-        for j in range(0,len(a)):
+        for j in range(len(a)):
             if (a[j][i] != a[ref_idx][i]) and ((a[ref_idx][i] in bases) and (a[j][i] in bases)) and a[j][i] not in alt:
                 alt.append(a[j][i])
         if len(alt) > 0:
-            print(chrom + "\t" + str(i+1) + "\t.\t" + a[ref_idx][i] + "\t", end="")
-            print(",".join(alt), end = "")
-            print("\t.\t.\t.\tGT\t", end = "")
+            row = [chrom, i+1, '.', a[ref_idx][i], ','.join(alt), '.', '.', '.', 'GT']
             vars = []
-            for k in range(0,len(a)):
+            for k in range(len(a)):
                 if a[k][i] == a[ref_idx][i]:
-                    vars.append("0")
+                    vars.append(0)
                 elif a[k][i] not in bases:
                     vars.append(".")
                 else:
                     for m in range(0, len(alt)):
                         if a[k][i] == alt[m]:
-                            vars.append(str(m+1))
-            print("\t".join(vars))
+                            vars.append(m+1)
+            yield row+vars
+            
 
 
 if __name__ == '__main__':
