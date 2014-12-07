afbe3fa426caa5413be71dee136c84367acb2d14
Danny Park <dpark@broadinstitute.org>
2014-12-06 20:45:58 -0500

    clean out unused code and command line calls to tabix

--- src/viral_ngs/tools/tabix.py
+++
@@ -1,5 +0,0 @@
-#       'tabix':        '/idi/sabeti-data/software/tabix/tabix-0.2.6',
-
-# (actually, we might not really need this tool if we rely on pysam?)
-# use instead: util.vcf.vcf_bgzip_index('inputfile.vcf', 'outputfile.vcf.gz')
-# and util.vcf.TabixReader and util.vcf.VcfReader
