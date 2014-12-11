04345ad7f2c8510f05e0ddf2192a6bb8f1d003bc
Danny Park <dpark@broadinstitute.org>
2014-12-11 16:45:04 -0500

    more info in hash [ci skip]

--- src/viral_ngs/broad_utils.py
+++ src/viral_ngs/broad_utils.py
@@ -112,7 +112,7 @@ __commands__.append(('extract_barcodes', main_extract_barcodes, parser_extract_b
 # ***  make_library_params   ***
 # ==============================
 
-def make_params_file(inFile, bamDir, outFile):
+def make_params_file(inFile, flowcell, lane, bamDir, outFile):
     header = ['BARCODE_1', 'BARCODE_2', 'OUTPUT', 'SAMPLE_ALIAS', 'LIBRARY_NAME', 'ID']
     with open(outFile, 'wt') as outf:
         outf.write('\t'.join(header)+'\n')
@@ -125,23 +125,24 @@ def make_params_file(inFile, bamDir, outFile):
                     'LIBRARY_NAME':row['sample']}
             if row.get('library_id_per_sample'):
                 out['LIBRARY_NAME'] += '.' + str(row['library_id_per_sample'])
-            out['OUTPUT'] = out['LIBRARY_NAME']
+            run_id = out['LIBRARY_NAME']
             if row.get('run_id_per_library'):
-                out['OUTPUT'] += '.r' + str(row['run_id_per_library'])
-            out['OUTPUT'] = os.path.join(bamDir, out['OUTPUT'] + ".bam")
-            out['ID'] = hashlib.sha1(out['OUTPUT'].encode('utf-8')).hexdigest().upper()[:8]
+                run_id += '.r' + str(row['run_id_per_library'])
+            out['OUTPUT'] = os.path.join(bamDir, run_id + ".bam")
+            out['ID'] = hashlib.sha1('{}.{}.{}'.format(run_id,flowcell,lane).encode('utf-8')).hexdigest().upper()[:8]
             outf.write('\t'.join(out[h] for h in header)+'\n')
 def parser_make_params_file() :
     parser = argparse.ArgumentParser(description='Create input file for illumina_basecalls')
     parser.add_argument('barcodeFile',
         help='''Input tab file w/header and four named columns:
                 barcode_name, library_name, barcode_sequence_1, barcode_sequence_2''')
-    parser.add_argument('run', help='Run number.', type=int)
-    parser.add_argument('bamDir', help='Directory for output bams.')
-    parser.add_argument('outFile', help='Output LIBRARY_PARAMS file for Picard.')
+    parser.add_argument('flowcell', help='Flowcell ID')
+    parser.add_argument('lane', help='Lane number', type=int)
+    parser.add_argument('bamDir', help='Directory for output bams')
+    parser.add_argument('outFile', help='Output LIBRARY_PARAMS file for Picard')
     return parser
 def main_make_params_file(args) :
-    make_params_file(args.barcodeFile, args.run, args.bamDir, args.outFile)
+    make_params_file(args.barcodeFile, args.flowcell, args.lane, args.bamDir, args.outFile)
     return 0
 __commands__.append(('make_params_file', main_make_params_file, parser_make_params_file))
 
