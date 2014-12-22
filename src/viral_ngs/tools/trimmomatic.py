2e8afb02df3317ff4063b8fac2ed3cdf1b5d1ad8
Danny Park <dpark@broadinstitute.org>
2014-12-22 16:07:26 -0500

    some cleanups in trimmomatic.py

--- src/viral_ngs/tools/trimmomatic.py
+++ src/viral_ngs/tools/trimmomatic.py
@@ -1,11 +1,6 @@
 "tools.Tool for trimmomatic."
 
-import tools, util.file
-import os, tempfile
-
-
-trimmomaticBroadUnixPath = '/idi/sabeti-scratch/kandersen/bin/trimmomatic/' \
-                                + 'trimmomatic-0.32.jar'
+import tools
 
 trimmomaticURL = 'http://www.usadellab.org/cms/uploads/supplementary/' \
                  'Trimmomatic/Trimmomatic-0.32.zip'
@@ -17,6 +12,4 @@ class TrimmomaticTool(tools.Tool) :
             install_methods.append(tools.DownloadPackage(trimmomaticURL,
                 'Trimmomatic-0.32/trimmomatic-0.32.jar',
                 require_executability=False))
-            #install_methods.append(tools.PrexistingUnixCommand(
-                #trimmomaticBroadUnixPath,require_executability=False))
         tools.Tool.__init__(self, install_methods = install_methods)
