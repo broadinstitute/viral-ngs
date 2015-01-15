73f97fbf692998f830f5fe89b66fd093565c9089
Danny Park <dpark@broadinstitute.org>
2015-01-15 01:54:40 -0500

    samtools name fix

--- assembly.py
+++ assembly.py
@@ -89,7 +89,7 @@ def order_and_orient(inFasta, inReference, outFasta, inReads=None):
     cmd = [os.path.join(util.file.get_scripts_path(), 'vfat', 'contigMerger.pl'),
         tmp_prefix+'_orientedContigs', inReference, tmp_prefix,
         '-musclepath', musclepath,
-        '-samtoolspath', tools.samtools.Samtools().install_and_get_path()]
+        '-samtoolspath', tools.samtools.SamtoolsTool().install_and_get_path()]
     if inReads:
         readsFq = list(map(util.file.mkstempfname, ('.1.fastq', '.2.fastq')))
         tools.picard.SamToFastqTool().execute(inReads, readsFq[0], readsFq[1])
