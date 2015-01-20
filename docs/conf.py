445b6ec303bc28e278ae2ca87ef2da433f699d51
Daniel Park <dpark@broadinstitute.org>
2015-01-19 20:35:51 -0500

    footer text injection works, just clean it up a bit [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -157,7 +157,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-html_last_updated_fmt = '%Y-%m-%d. Code version {}.'.format(release)
+html_last_updated_fmt = '%Y-%m-%d.<br>Code version {}'.format(release)
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
