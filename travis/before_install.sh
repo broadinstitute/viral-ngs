ddf00c31bb3af9bae5c995a49df5d9a51a4a1b43
Danny Park <dpark@broadinstitute.org>
2015-02-09 13:44:46 -0500

    fix symlink for docker cache

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -3,15 +3,13 @@
 # cache depending on whether we're on the master branch or not.
 set -e
 
-rm -rf tools/build
-mkdir -p caches/tools_build
-
 if [ "$TRAVIS_BRANCH" != "master" ]; then
     echo "Travis docker caches allowed for branch $TRAVIS_BRANCH"
-    ln -s caches/tools_build tools/build
+    rm -rf tools/build
+    mkdir -p $HOME/caches/tools_build
+    ln -s $HOME/caches/tools_build tools/build
 
 else
     echo "Travis docker cache disabled for tools/build on master branch"
-    mkdir -p tools/build
 
 fi
