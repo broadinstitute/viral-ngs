4812c43ebc927a4cfa47375cf35638d5dd4f6e1a
Danny Park <dpark@broadinstitute.org>
2015-02-09 10:54:08 -0500

    attempt to figrue out why py3 detection and bin_bundle caching arent working

--- travis/install-pip.sh
+++ travis/install-pip.sh
@@ -5,7 +5,7 @@ echo "pip installing required python packages"
 pip install -r requirements.txt
 
 PYVER=`python --version | cut -c 8`
-if [ $PYVER == "3" ]; then
+if [ "$PYVER" == "3" ]; then
     echo "pip installing snakemake packages (py3 only)"
     pip install -r requirements-pipes.txt
 fi
