de519b740003f4ea7fcc7cbf106b55ee1ee9c2fe
Danny Park <dpark@broadinstitute.org>
2015-02-09 10:54:08 -0500

    attempt to figrue out why py3 detection and bin_bundle caching arent working

--- travis/install-tools.sh
+++ travis/install-tools.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ ! -d $GATK_PATH -o ! -d $NOVOALIGN_PATH ]; then
     echo "Fetching encrypted Novoalign & GATK bundle for Travis"
+    pwd
     wget http://www.broadinstitute.org/~dpark/viral_ngs-gatk_novoalign-encrypted_for_travis.tar.gz.enc
     openssl aes-256-cbc -d -k "$BUNDLE_SECRET" -in viral_ngs-gatk_novoalign-encrypted_for_travis.tar.gz.enc -out bin_bundles.tar.gz
     tar -xzpvf bin_bundles.tar.gz
