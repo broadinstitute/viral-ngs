d1cf97d6805c2f1f02afde59dfcdbb5bce5aebcf
Danny Park <dpark@broadinstitute.org>
2015-02-09 14:36:15 -0500

    more flex in before_install script

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -3,6 +3,13 @@
 # cache depending on whether we're on the master branch or not.
 set -e
 
+# Always cache the encrypted bin_bundles that come from the Broad
+# (pull requests from forks don't see the Travis secret key, so this
+# allows external PRs to get tested properly)
+mkdir -p $HOME/caches/bin_bundles
+ln -s $HOME/caches/bin_bundles bin_bundles
+
+# Only sometimes cache the tools/build directory
 if [ "$TRAVIS_BRANCH" != "master" ]; then
     echo "Travis docker caches allowed for branch $TRAVIS_BRANCH"
     rm -rf tools/build
@@ -13,3 +20,7 @@ else
     echo "Travis docker cache disabled for tools/build on master branch"
 
 fi
+
+# Always report how big things are
+echo "Docker cache space usage:"
+du -hs $HOME/virtualenv $HOME/caches/*
