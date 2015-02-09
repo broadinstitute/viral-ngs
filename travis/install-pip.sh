7fd08ae54bae5b25686478d47762b36007bacb73
Danny Park <dpark@broadinstitute.org>
2015-02-09 11:03:28 -0500

    switch from sh to bash

--- travis/install-pip.sh
+++ travis/install-pip.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 echo "pip installing required python packages"
 pip install -r requirements.txt
 
-PYVER=`python --version | cut -c 8`
-if [ "$PYVER" == "3" ]; then
+if [ `python --version | cut -c 8` = "3" ]; then
     echo "pip installing snakemake packages (py3 only)"
     pip install -r requirements-pipes.txt
 fi
