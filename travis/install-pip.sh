59cce70a2909fc1cdc28645ed2f4e803978b3e2e
Danny Park <dpark@broadinstitute.org>
2015-02-09 10:18:23 -0500

    cache Novoalign/GATK bundle as well, install snakemake if py3, push some travis.yml stuff into sub-scripts, prepare a place to put long running integration tests. Destroy cache of tools on pull requests. Closes #69

---
+++ travis/install-pip.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "pip installing required python packages"
+pip install -r requirements.txt
+
+if [ `python --version | cut -c 8` == "3" ]; then
+    echo "pip installing snakemake packages (py3 only)"
+    pip install -r requirements-pipes.txt
+fi
+
+echo "pip installing coveralls"
+pip install -q coveralls nose-cov
---
+++ travis/tests-unit.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+nosetests -v --with-xunit --with-coverage --nocapture \
+    --cover-erase --cover-inclusive --cover-branches --cover-tests \
+    --cover-package broad_utils,assembly,interhost,intrahost,ncbi,read_utils,reports,taxon_filter,tools,util
