d592503306c2ed9d290280041be48ec78f03c929
Danny Park <dpark@broadinstitute.org>
2015-02-16 15:17:07 -0500

    add some dry-run snakemake tests

---
+++ tests/unit/test_snake.py
@@ -0,0 +1,77 @@
+# Unit tests for snakemake pipelines
+
+__author__ = "dpark@broadinstitute.org"
+
+import util.cmd, util.file
+import unittest, argparse
+import sys, os, subprocess, shutil, tempfile, argparse, itertools
+from test import TestCaseWithTmp
+
+if sys.version_info>=(3,2):
+    import snakemake
+
+
+def setup_dummy_simple(
+    sample_names=('G1234', 'G5678', 'G3671.1_r1', 'G3680-1_4',
+        '9876', 'x.y-7b')):
+    ''' Set up a very simple project directory with empty input files. '''
+    
+    workdir = tempfile.mkdtemp()
+    os.mkdir(os.path.join(workdir, 'data'))
+    os.mkdir(os.path.join(workdir, 'data', '00_raw'))
+    os.mkdir(os.path.join(workdir, 'log'))
+    os.mkdir(os.path.join(workdir, 'reports'))
+    os.mkdir(os.path.join(workdir, 'tmp'))
+    
+    for s in sample_names:
+        with open(os.path.join(workdir, 'data', '00_raw', s+'.bam'), 'wt') as outf:
+            pass
+    for fn in ('samples-assembly.txt', 'samples-depletion.txt', 'samples-runs.txt'):
+        with open(os.path.join(workdir, fn), 'wt') as outf:
+            for s in sample_names:
+                outf.write(s+'\n')
+    
+    shutil.copy(os.path.join(util.file.get_project_path(),
+        'pipes', 'Snakefile'), workdir)
+    shutil.copy(os.path.join(util.file.get_project_path(),
+        'pipes', 'config.json'), workdir)
+    
+    os.symlink(util.file.get_project_path(),
+        os.path.join(workdir, 'bin'))
+    
+    return workdir
+
+
+@unittest.skipIf(sys.version_info<(3,2), "python version is too old for snakemake")
+class TestSimpleDryRuns(TestCaseWithTmp):
+    def setUp(self):
+        TestCaseWithTmp.setUp(self)
+        self.workdir = setup_dummy_simple()
+
+    def test_dryrun_all(self):
+        ''' Test that the "all" rule dryruns properly '''
+        self.assertTrue(snakemake.snakemake(
+            os.path.join(self.workdir, 'Snakefile'),
+            configfile=os.path.join(self.workdir, 'config.json'),
+            workdir=self.workdir, dryrun=True))
+        self.assertTrue(snakemake.snakemake(
+            os.path.join(self.workdir, 'Snakefile'),
+            configfile=os.path.join(self.workdir, 'config.json'),
+            workdir=self.workdir, dryrun=True,
+            targets=['all']))
+
+    def test_dryrun_all_assemble(self):
+        ''' Test that the "all_assemble" rule dryruns properly '''
+        self.assertTrue(snakemake.snakemake(
+            os.path.join(self.workdir, 'Snakefile'),
+            configfile=os.path.join(self.workdir, 'config.json'),
+            workdir=self.workdir, dryrun=True,
+            targets=['all_assemble']))
+
+    def test_dryrun_all_deplete(self):
+        ''' Test that the "all_deplete" rule dryruns properly '''
+        self.assertTrue(snakemake.snakemake(
+            os.path.join(self.workdir, 'Snakefile'),
+            configfile=os.path.join(self.workdir, 'config.json'),
+            workdir=self.workdir, dryrun=True,
+            targets=['all_deplete']))
--- travis/install-pip.sh
+++ travis/install-pip.sh
@@ -4,9 +4,8 @@ set -e
 echo "pip installing required python packages"
 pip install -r requirements.txt
 
-echo "TRAVIS_PYTHON_VERSION: $TRAVIS_PYTHON_VERSION"
-
-PYVER=`python -V 2>&1 | cut -c 8`
+#PYVER=`python -V 2>&1 | cut -c 8`
+PYVER=`echo $TRAVIS_PYTHON_VERSION | cut -c 1`
 if [ "$PYVER" = "3" ]; then
     echo "pip installing snakemake packages (py3 only)"
     pip install -r requirements-pipes.txt
