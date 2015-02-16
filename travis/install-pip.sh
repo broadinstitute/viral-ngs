1f4d14a6c7ae57c55fa3dfb29b456b9e906c3dba
Danny Park <dpark@broadinstitute.org>
2015-02-16 14:17:08 -0500

    test to see how TRAVIS_PYTHON_VERSION is set

--- travis/install-pip.sh
+++ travis/install-pip.sh
@@ -4,6 +4,8 @@ set -e
 echo "pip installing required python packages"
 pip install -r requirements.txt
 
+echo "TRAVIS_PYTHON_VERSION: $TRAVIS_PYTHON_VERSION"
+
 PYVER=`python -V 2>&1 | cut -c 8`
 if [ "$PYVER" = "3" ]; then
     echo "pip installing snakemake packages (py3 only)"
