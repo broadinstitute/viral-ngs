45e0f49dbd6156ba2c2ef74f5834e737610e33b1
Danny Park <dpark@broadinstitute.org>
2015-02-19 17:09:06 -0500

    some initial isnv test framework

--- tests/unit/phylo/test_interhost.py
+++ tests/unit/phylo/test_interhost.py
@@ -3,10 +3,27 @@
 __author__ = "PLACEHOLDER"
 
 import interhost
-import unittest, argparse
+import test
+import unittest, shutil, argparse
 
 class TestCommandHelp(unittest.TestCase):
     def test_help_parser_for_each_command(self):
         for cmd_name, parser_fun in interhost.__commands__:
             parser = parser_fun(argparse.ArgumentParser())
             helpstring = parser.format_help()
+
+
+@unittest.skip('not implemented')
+class TestCoordMapper(test.TestCaseWithTmp):
+    def setUp(self):
+        super(TestCoordMapper, self).setUp()
+        pass
+    def test_one_chr_no_gaps(self):
+        pass
+    def test_one_chr_gaps(self):
+        pass
+    def test_two_chr_no_gaps(self):
+        pass
+    
+    
+    
