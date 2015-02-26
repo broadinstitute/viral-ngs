5eab528627ff46d9c4352b5f31cca036e16f87d6
Danny Park <dpark@broadinstitute.org>
2015-02-26 11:53:33 -0500

    travis: run long tests on tagged releases

--- travis/tests-long.sh
+++ travis/tests-long.sh
@@ -4,7 +4,7 @@ set -e
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 
-if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH = "master" ]; then
+if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH = "master" -o -n "$TRAVIS_TAG" ]; then
     echo "This is on master or is a pull request: executing long running tests..."
     nosetests -v --with-xunit --with-coverage --nocapture \
         --cover-inclusive --cover-branches --cover-tests \
