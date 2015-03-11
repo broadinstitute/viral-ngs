d2c33d44487e69ab5007096be599e048549e7868
Irwin Jungreis <iljungr@csail.mit.edu>
2015-03-11 13:10:03 -0400

    Added unit test test_vphaser_one_sample_indels. Allowed minReadsEach=0 in vphaser_one_sample, in which case maxBias is ignored.

--- tests/unit/assemble/test_intrahost.py
+++ tests/unit/assemble/test_intrahost.py
@@ -148,6 +148,26 @@ class TestPerSample(test.TestCaseWithTmp):
         expected = os.path.join(myInputDir, 'vphaser_one_sample_expected.txt')
         self.assertEqualContents(outTab, expected)
 
+    def test_vphaser_one_sample_indels(self):
+        # Files here were created as follows:
+        # ref.indels.fasta is Seed-stock-137_S2_L001_001.fasta
+        # in.indels.bam was created from Seed-stock-137_S2_L001_001.mapped.bam
+        #     as follows:
+        # Created two .sam files using samtools view, restricting to ranges
+        # 6811-7011 and 13081-13281, respectively. Paired reads were removed
+        # from those files by throwing out the second occurence of any read name
+        # and anding the flag fields with 16. Then, a random 90% of reads were
+        # removed, except that any reads containing the indel variants at
+        # 6911 and 13181 were kept. Then the resulting 2 files were combined.
+        myInputDir = util.file.get_test_input_path(self)
+        inBam = os.path.join(myInputDir, 'in.indels.bam')
+        refFasta = os.path.join(myInputDir, 'ref.indels.fasta')
+        outTab = util.file.mkstempfname('.txt')
+        intrahost.vphaser_one_sample(inBam, refFasta, outTab,
+            vphaserNumThreads = 4, minReadsEach = 0)
+        expected = os.path.join(myInputDir, 'vphaser_one_sample_indels_expected.txt')
+        self.assertEqualContents(outTab, expected)
+
     def test_vphaser_one_sample_2libs(self):
         # in.2libs.bam was created by "manually" editing in.bam and moving about
         # 1/3 of the reads to ReadGroup2.
@@ -165,7 +185,7 @@ class TestPerSample(test.TestCaseWithTmp):
         #    the chi2_contingency approximation to fisher_exact. The 4th, 5th,
         #    and 6th rows have large enough minor allele count that their
         #    p-values are calculated using the chi2 approximation. The other
-        #    rows are testing fisher_exact for the 2 x 3 case.
+        #    rows are testing the 2 x 3 case of fisher_exact.
         # in.3libs.bam was created by "manually" editing in.2libs.bam and moving
         # about 1/2 of the reads in ReadGroup2 to ReadGroup3.
         myInputDir = util.file.get_test_input_path(self)
