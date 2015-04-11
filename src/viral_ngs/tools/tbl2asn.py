466c5276f2242899298458e081e73f30af1d23bf
Danny Park <dpark@broadinstitute.org>
2015-04-10 21:52:34 -0400

    implement tbl2asn tool

--- src/viral_ngs/tools/tbl2asn.py
+++ src/viral_ngs/tools/tbl2asn.py
@@ -6,25 +6,46 @@
 __author__ = "dpark@broadinstitute.org"
 
 import logging, tools, util.file
-import os, os.path, subprocess
+import os, os.path, subprocess, gzip
 
-url = 'ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/'
+url = 'ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/{os}.tbl2asn.gz'
 
 log = logging.getLogger(__name__)
 
 class Tbl2AsnTool(tools.Tool):
     def __init__(self, install_methods = None):
         if install_methods == None:
-            install_methods = []
-        fname = "{}.tbl2asn.gz".format(get_bintype())
-        raise NotImplementedError()
+            install_methods = [DownloadGzipBinary(url.format(get_bintype()), 'tbl2asn')]
         tools.Tool.__init__(self, install_methods = install_methods)
 
     def version(self):
         return None
 
-    def execute(self, args):
-        pass
+    def execute(self, inputDir, outputDir, templateFile,
+            source_quals=[], comment=None, verification='vb',
+            file_type='s'):
+        
+        toolCmd = [self.install_and_get_path()]
+        
+        if inputDir:
+            toolCmd += ['-p', inputDir]
+        if outputDir:
+            toolCmd += ['-r', outputDir]
+        if templateFile:
+            toolCmd += ['-t', templateFile]
+        if source_quals:
+            toolCmd.append('-j')
+            toolCmd.append(' '.join("[{}={}]".format(k,v) for k,v in source_quals))
+        if comment:
+            toolCmd += ['-y', comment]
+        if verification:
+            toolCmd += ['-v', verification]
+        if file_type:
+            toolCmd += ['-a', file_type]
+            
+        log.debug(' '.join(toolCmd))
+        subprocess.check_call(toolCmd)
+
 
 def get_bintype():
     uname = os.uname()
@@ -38,6 +59,12 @@ def get_bintype():
     else:
         raise Exception('unsupported OS')
 
-
-
-
+class DownloadGzipBinary(tools.DownloadPackage):
+    def unpack(self, download_dir):
+        util.file.mkdir_p(self.destination_dir)
+        if (self.download_file.endswith('.gz')
+            and not self.download_file.endswith('.tar.gz')):
+            with gzip.open(os.path.join(download_dir, self.download_file)) as inf:
+                with open(self.targetpath, 'wb') as outf:
+                    outf.write(inf.read())
+            os.chmod(self.targetpath, 0o755)
