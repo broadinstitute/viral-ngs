53bed3b101ad34ed8ec9fb72a6e89500652a6395
Danny Park <dpark@broadinstitute.org>
2015-04-10 23:54:50 -0400

    add prep_genbank_files

--- src/viral_ngs/ncbi.py
+++ src/viral_ngs/ncbi.py
@@ -6,59 +6,14 @@ to NCBI's Genbank and SRA databases.
 __author__ = "PLACEHOLDER"
 __commands__ = []
 
-import argparse, logging, collections, shutil, os, os.path
+import argparse, logging, collections, shutil, os, os.path, glob
 import Bio.SeqIO
 import util.cmd, util.file, util.vcf, util.misc
+import tools.tbl2asn
 import interhost
 
 log = logging.getLogger(__name__)
 
-
-def tbl_reader(text_lines):
-    ''' This function reads an iterator of unformatted text lines that represent
-        features in NCBI's "tbl" format.  It returns them as an iterator of
-        features:
-            (seqid, start, stop, type, child_records)
-        child_records is an ordered list of tuples:
-            (key, value)
-        This function accepts files that describe multiple sequences.
-    '''
-    (cur_seqid, start, stop, ftype, children) = (None, None, None, None, None)
-    for line_num, line in enumerate(text_lines):
-        try:
-            line = line.rstrip('\n\r')
-            if not line:
-                pass
-            elif line.startswith('>'):
-                # new sequence
-                if not line.startswith('>Feature '):
-                    raise Exception("not sure how to handle a non-Feature record")
-                else:
-                    if cur_seqid and start and stop:
-                        yield (cur_seqid, start, stop, ftype, children)
-                    cur_seqid = line[len('>Feature '):].strip()
-                    start, stop = (None, None)
-            elif line[0] not in (' ', '\t'):
-                # new feature
-                if not cur_seqid:
-                    raise Exception("feature started before the sequence id was specified")
-                if cur_seqid and start and stop:
-                    yield (cur_seqid, start, stop, ftype, children)
-                start, stop, ftype = line.split('\t')
-                start = int(start)
-                stop = int(stop)
-                children = []
-            else:
-                # child info
-                k, v = line.strip().split('\t')
-                children.append((k,v))
-        except:
-            log.exception("error parsing tbl file at line {}, contents: '{}'".format(
-                line_num, line))
-            raise
-    # at EOF, emit last feature
-    if cur_seqid and start and stop:
-        yield (cur_seqid, start, stop, ftype, children)
 
 
 def fasta_chrlens(fasta):
@@ -151,6 +106,8 @@ __commands__.append(('tbl_transfer', parser_tbl_transfer))
 
 
 def fasta2fsa(infname, outdir):
+    ''' copy a fasta file to a new directory and change its filename to end in .fsa
+        for NCBI's sake. '''
     outfname = os.path.basename(infname)
     if outfname.endswith('.fasta'):
         outfname = outfname[:-6]
@@ -161,6 +118,35 @@ def fasta2fsa(infname, outdir):
     outfname = os.path.join(outdir, outfname)
     shutil.copyfile(infname, outfname)
     return outfname
+
+def prep_genbank_files(templateFile, indir, outdir, master_source_table=None, comment=None):
+    ''' prepare genbank submission files '''
+    # make output directory
+    util.file.mkdir_p(outdir)
+    for fn in glob.glob(os.path.join(indir, '*.fasta')):
+        fasta2fsa(fn, outdir)
+    for fn in glob.glob(os.path.join(indir, '*.tbl')):
+        shutil.copy(fn, outdir)
+        if master_source_table:
+            srcfile = fn[:-4] + '.src'
+            shutil.copy(master_source_table, srcfile)
+        
+    # run tbl2asn
+    tbl2asn = tools.tbl2asn.Tbl2AsnTool()
+    tbl2asn.execute(templateFile, outdir, comment=comment)
+
+def parser_prep_genbank_files(parser=argparse.ArgumentParser()):
+    parser.add_argument('templateFile', help='Template file (.sbt)')
+    parser.add_argument("indir", help="Input directory with fasta and tbl files")
+    parser.add_argument("outdir", help="Output directory with genbank submission files")
+    parser.add_argument('--comment', default=None,
+        help='comment field')
+    parser.add_argument('--master_source_table', default=None,
+        help='source modifier table')
+    util.cmd.common_args(parser, (('tmpDir',None), ('loglevel',None), ('version',None)))
+    util.cmd.attach_main(parser, prep_genbank_files, split_args=True)
+    return parser
+__commands__.append(('prep_genbank_files', parser_prep_genbank_files))
 
 
 def full_parser():
