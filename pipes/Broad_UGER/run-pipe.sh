455dc8b3981dcc9687b419a8720a9f411d22dd4f
Chris Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-07-28 16:32:51 -0400

    Added subdir to pipes/ for Broad_UGER,
    cluster-submitter.py and run-pipe.sh have been brought over from
    Broad_LSF, and mostly adapted to use the qsub command and its
    equivalent parameters

---
+++ pipes/Broad_UGER/run-pipe.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Wrappers around Snakemake for use on the Broad LSF cluster
+
+# load necessary Broad dotkits
+eval `/broad/software/dotkit/init -b`
+reuse -q UGER
+reuse -q Python-3.4
+reuse -q Perl-5.10
+
+# load config dirs from config.json
+VENVDIR=`python -c 'import json;f=open("config.json");print(json.load(f)["venvDir"]);f.close()'`
+BINDIR=`python -c 'import json;f=open("config.json");print(json.load(f)["binDir"]);f.close()'`
+
+# load Python virtual environment
+source "$VENVDIR/bin/activate"
+
+# invoke Snakemake in cluster mode with custom wrapper scripts
+snakemake --timestamp --rerun-incomplete --keep-going --nolock \
+    --jobs 100000 --immediate-submit \
+        --latency-wait 20 \
+    --config mode=UGER job_profiler="$BINDIR/pipes/Broad_LSF/lsf-report.py" \
+    --directory . \
+    --jobscript "$BINDIR/pipes/Broad_UGER/jobscript.sh" \
+    --cluster $BINDIR'/pipes/Broad_UGER/cluster-submitter.py {dependencies} {config[logDir]}' \
+    "$@"
