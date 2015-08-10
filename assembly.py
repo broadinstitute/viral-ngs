64a2877fbaf17425050ed7423902a48e6defdb51
Chris Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-08-10 14:36:39 -0400

    syntax fix

--- assembly.py
+++ assembly.py
@@ -287,7 +287,7 @@ def impute_from_reference(inFasta, inReference, outFasta,
                     '--replace-end-gaps']
                 if newName:
                     # TODO: may need to add/remove the "-idx" for downstream
-                    args.extend(['-n', newName]+"-"+str(idx)])
+                    args.extend(['-n', newName+"-"+str(idx)])
 
                 args = pmc.parse_args(args)
                 args.func_main(args)
