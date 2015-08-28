69b25e143c4ebd27572a0f9323e560d4d091cdd7
Chris Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-08-27 21:12:24 -0400

    added filesystem check before snakemake jobscript command, to return 99 to reschedule a job in the event that the dataDir is unavailable (as in the case of certain nodes on UGER when NFS is down)

--- pipes/Broad_UGER/jobscript.sh
+++ pipes/Broad_UGER/jobscript.sh
@@ -3,8 +3,15 @@
 # this is identical to the default jobscript with the exception of the exit code
 
 BINDIR=`python -c 'import json; import os; f=open("config.json");print(os.path.realpath(json.load(f)["binDir"]));f.close()'`
+DATADIR=`python -c 'import json; import os; f=open("config.json");print(os.path.realpath(json.load(f)["dataDir"]));f.close()'`
 
 source "$BINDIR/pipes/Broad_UGER/setup_dotkits.sh"
+
+# if listing the data directory fails, exit 99 to reschedule the job
+# since the node with the job doesn't have the NFS share mounted.
+# Hopefully it will be sent to a node that has it mounted.
+# For this to work, re-run must be set to y via "qsub -r y"
+ls "$DATADIR" || exit 99
 
 {exec_job}
 
