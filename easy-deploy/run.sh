8e24b5461b58ff521a00b6ce2a6abeaa6a0eb4d7
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-09-28 19:27:30 -0400

    improvements to run script, increased VM RAM,

--- easy-deploy/run.sh
+++ easy-deploy/run.sh
@@ -42,9 +42,39 @@ function cmd_exists() {
     fi
 }
 
+function all_commands_exist {
+    # Pass in a space-delimited string of commands
+    # to try. Returns 0 if all exist
+    #
+    # $1 array of commands to try
+
+    all_exist=true
+
+    cmds_array=($(echo $1 | tr " " "\n"))
+
+    for f in "${cmds_array[@]}"; do
+        if ! cmd_exists $f; then
+            echo "Command missing: $f"
+            if $all_exist; then
+                all_exist=false
+            fi
+        fi
+    done
+
+    if $all_exist; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 function install_with_blocking() {
+    # Use to run an install command where there isn't an easy way
+    # to see if the command already exists
+    #
     # $1 the install command to run
     # $2 description of thing to be installed
+
     if eval $1 ; then
         return 0
     else
@@ -54,6 +84,9 @@ function install_with_blocking() {
 }
 
 function use_or_install_with_checks() {
+    # Used to run an install command if a specified command does
+    # not exist
+    #
     # $1 command to check to see if it is already installed
     # $2 description of the package
     # $3 command to install it    
@@ -90,7 +123,6 @@ function clean_up {
 }
 trap clean_up SIGHUP SIGINT SIGTERM
 
-#echo $'viral-ngs easy deployment script\n'
 echo $' +-----------------------------------------------------------+ '
 echo $' |       _   _ _           _        _   _ _____  _____       | '
 echo $' |      | | | (_)         | |      | \ | |  __ \/  ___|      | '
@@ -111,31 +143,72 @@ echo $' |                  |___/             |_|            |___/   | '
 echo $' |                                                           | '
 echo $' +-----------------------------------------------------------+ '
 
+relevant_commands=('ansible vagrant VirtualBox')
 
-if ! cmd_exists "vagrant" || ! cmd_exists "VirtualBox"; then
+if ! all_commands_exist $relevant_commands; then
     if $(ask "Install local dependencies for running viral-ngs?" Y); then
         echo "Setting up dependencies..."
+
+        if [ -z "$GATK_PATH" ]; then 
+            echo "Prior to setting up everything, you must license and download GATK ( https://www.broadinstitute.org/gatk/download/ )." 
+            echo "Once downloaded, extract the archive and set the environment variable GATK_PATH to the directory path"
+            echo "containing 'GenomeAnalysisTK.jar'"
+            echo "  ex. 'export GATK_PATH=/path/to/gatk'"
+            echo "       Note: add the export line to .bashrc or .bash_profile for persistence across sessions."
+            exit 1;
+        else
+            echo "GATK_PATH is set to '$GATK_PATH'"
+            echo "Continuing..."
+        fi
+
+        if [ -z "$NOVOALIGN_PATH" ]; then 
+            echo "Prior to setting up everything, you must license and download NovoAlign ( http://www.novocraft.com/products/novoalign/ )." 
+            echo "Once downloaded, extract the archive and set the environment variable NOVOALIGN_PATH to the directory path"
+            echo "containing 'novoalign'"
+            echo "  ex. 'export NOVOALIGN_PATH=/path/to/novoalign'"
+            echo "       Note: add the export line to .bashrc or .bash_profile for persistence across sessions."
+            exit 1;
+        else
+            echo "NOVOALIGN_PATH is set to '$NOVOALIGN_PATH'"
+            echo "Continuing..."
+        fi
 
         if [ "$(uname)" == "Darwin" ]; then
             echo "Platform: Mac"
 
             if use_or_install_with_checks "brew" "Homebrew package mananger" 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ' 1 ; then
                 echo "Homebrew found, installing cask..."
+                use_or_install_with_checks 'ansible' "Ansible deploy and config manager" 'brew install ansible' 1
                 if use_or_install_with_checks 'brew-cask' "Homebrew cask" 'brew install caskroom/cask/brew-cask' 1 ; then
-                    install_with_blocking 'brew cask install virtualbox'
-                    install_with_blocking 'brew cask install vagrant'
-                    install_with_blocking 'brew cask install vagrant-manager'
-                fi        
+                    #install_with_blocking 'brew cask install virtualbox'
+                    use_or_install_with_checks 'VirtualBox' "VirtualBox VM manager" 'brew cask install virtualbox' 1
+                    #install_with_blocking 'brew cask install vagrant'
+                    use_or_install_with_checks 'vagrant' "Vagrant VM provisioning tool" 'brew cask install vagrant' 1
+                    install_with_blocking 'brew cask install vagrant-manager' 'vagrant-manager'
+                fi          
+                # if the version of ruby is not 2.0 ( OSX < 10.10 )
+                if ! echo $(ruby -e 'print RUBY_VERSION') | grep "2."; then
+                    echo "Ruby is not at version 2.x. Installing 2.0 from Homebrew..."
+                    install_with_blocking "brew install ruby20" "Ruby 2.0"
+                fi
+                install_with_blocking "vagrant plugin install vagrant-aws" 'vagrant-aws'
             fi
 
         elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
             echo "Platform: Linux"
 
-            if [ $(python -mplatform | grep Ubuntu) ]; then
+            if cmd_exists "apt-get"; then
                 use_or_install_with_checks "VirtualBox" "VirtualBox VM manager" 'sudo apt-get -y install virtualbox virtualbox-dkms' 1
-                use_or_install_with_checks "vagrant" "Vagrant VM environment manager" 'sudo apt-get -y install vagrant' 1
+                use_or_install_with_checks "ansible" "Ansible deploy and config manager" 'sudo apt-get -y install ansible' 1
+                use_or_install_with_checks "vagrant" "Vagrant VM provisioning tool" 'sudo apt-get -y install vagrant' 1
+
+                if ! echo $(ruby -e 'print RUBY_VERSION') | grep "2."; then
+                    echo "Please update your system ruby to >2.0 and re-run this script"
+                else
+                    install_with_blocking '$(vagrant plugin install vagrant-aws)' 'vagrant-aws'
+                fi
             else
-                echo "This script is intended to be used with Ubuntu. If you are using a different Linux distro, please set up the environment manually."
+                echo "This script is intended to be used on a system with apt. If you are using a Linux distro without apt, please set up the environment manually."
                 exit 1
             fi
 
@@ -147,30 +220,66 @@ if ! cmd_exists "vagrant" || ! cmd_exists "VirtualBox"; then
         echo "Aborting."
         exit 1
     fi
-    install_with_blocking "vagrant plugin install vagrant-aws"
-else
+fi
+
+if all_commands_exist $relevant_commands; then
     echo $'Dependencies for building the viral-ngs environment are SATISFIED.\n'
     echo $'Next you have an opportunity to choose whether you would like to provision'
     echo $'the viral-ngs environment LOCALLY or on AWS EC2.'
     echo $'In either case, you will be directed to an ssh session on a VM where'
     echo $'you can work with viral-ngs.\n'
 
-    if $(ask "Run viral-ngs LOCALLY?" Y); then
-        vagrant up --provider=virtualbox
-        vagrant box update
-        vagrant ssh
-        vagrant suspend
-        if [[ $? ]]; then
-            echo $'\nThe Viral-NGS virtual machine has been suspended. Next time you run this'
-            echo $'script, you will be connected exactly where you left off.\n'
-            exit 0
-        else
-            echo $'The Vial-NGS virtual machine did not close cleanly.'
-            exit 1
-        fi
-    elif $(ask "Run viral-ngs on AWS?" Y); then
-        vagrant up --provider=aws
-        vagrant ssh
-        exit 0
+    # Ask the question
+    read -p "Run viral-ngs locally or on AWS EC2? [LOCAL/aws] " REPLY
+    
+    # Default?
+    if [ -z "$REPLY" ]; then
+        REPLY="local"
     fi
+    
+    # Check if the reply is valid
+    case "$REPLY" in
+        L*|l*) # could also be explicit LOCAL|local)
+            echo "Running locally..."; 
+            vagrant up --provider=virtualbox
+            vagrant box update
+            vagrant ssh
+            vagrant suspend
+            if [[ $? ]]; then
+                echo $'\nThe Viral-NGS virtual machine has been suspended. Next time you run this'
+                echo $'script, you will be connected exactly where you left off.\n'
+                exit 0
+            else
+                echo $'The Vial-NGS virtual machine did not close cleanly.'
+                exit 1
+            fi
+            ;;
+        A*|a*) 
+            echo "AWS support not yet implemented."
+            exit 1
+
+            echo "Running on AWS...";
+
+            if ! echo $(ruby -e 'print RUBY_VERSION') | grep "2."; then
+                echo "Running viral-ngs on AWS needs ruby >2.0"
+                exit 1
+            else
+                if ! $(vagrant plugin list | grep aws); then
+                    install_with_blocking "vagrant plugin install vagrant-aws" 'vagrant-aws'
+                fi
+            fi
+
+            vagrant up --provider=aws
+            vagrant ssh
+            vagrant suspend
+            if [[ $? ]]; then
+                echo $'\nThe Viral-NGS virtual machine has been suspended. Next time you run this'
+                echo $'script, you will be connected exactly where you left off.\n'
+                exit 0
+            else
+                echo $'The Vial-NGS virtual machine did not close cleanly.'
+                exit 1
+            fi
+            ;;
+    esac
 fi
