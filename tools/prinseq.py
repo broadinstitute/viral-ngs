ed5006c1af1dc0f488cfda2ff5b5122d945d4e40
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-11-30 11:37:15 -0500

    updated prinseq version for fixes and support for Perl 5.14+
    updated prinseq version for fixes and support for Perl 5.14+

--- tools/prinseq.py
+++ tools/prinseq.py
@@ -2,8 +2,9 @@
 
 import tools
 
+tool_version = '0.20.4'
 url = 'http://sourceforge.net/projects/prinseq/files/standalone/' \
-      'prinseq-lite-0.19.3.tar.gz'
+      'prinseq-lite-{ver}.tar.gz'.format(ver=tool_version)
 
 
 class PrinseqTool(tools.Tool):
@@ -11,7 +12,7 @@ class PrinseqTool(tools.Tool):
     def __init__(self, install_methods=None):
         if install_methods is None:
             install_methods = []
-            target_rel_path = 'prinseq-lite-0.19.3/prinseq-lite.pl'
+            target_rel_path = 'prinseq-lite-{ver}/prinseq-lite.pl'.format(ver=tool_version)
             install_methods.append(
                 tools.DownloadPackage(url,
                                       target_rel_path,
