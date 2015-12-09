f44f711e2f760d2650d35e9a73214b69fbc81804
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-08 19:43:55 -0500

    add conda install_method for prinseq

--- tools/prinseq.py
+++ tools/prinseq.py
@@ -2,6 +2,7 @@
 
 import tools
 
+TOOL_NAME = "prinseq"
 TOOL_VERSION = '0.20.4'
 TOOL_URL = 'http://sourceforge.net/projects/prinseq/files/standalone/' \
       'prinseq-lite-{ver}.tar.gz'.format(ver=TOOL_VERSION)
@@ -12,6 +13,8 @@ class PrinseqTool(tools.Tool):
     def __init__(self, install_methods=None):
         if install_methods is None:
             install_methods = []
+            install_methods.append( tools.CondaPackage(TOOL_NAME, version=TOOL_VERSION) )
+            
             target_rel_path = 'prinseq-lite-{ver}/prinseq-lite.pl'.format(ver=TOOL_VERSION)
             install_methods.append(
                 tools.DownloadPackage(TOOL_URL,
