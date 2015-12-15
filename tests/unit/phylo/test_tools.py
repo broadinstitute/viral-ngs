b38dab4118d30d4d354da6e618202671254ce538
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-15 17:01:33 -0500

    remove debug lines from test_tools

--- tests/unit/phylo/test_tools.py
+++ tests/unit/phylo/test_tools.py
@@ -12,9 +12,10 @@ import logging
 import util.cmd
 import util.file
 from test import TestCaseWithTmp
+import functools
 
 log = logging.getLogger(__name__)
-
+util.cmd.setup_logger('INFO')
 
 class TestToolsInstallation(TestCaseWithTmp):
 
@@ -38,11 +39,10 @@ class TestToolsInstallation(TestCaseWithTmp):
         '''Load every tool's default chain of install methods and try them.'''
         for tool_class in iter_leaf_subclasses(tools.Tool):
             t = tool_class()
-            if hasattr(t, "is_installed"):
-                t.install()
-                self.assertTrue(t.is_installed(), "installation of tool %s failed" % tool_class.__name__)
-                log.info(".. installation of %s succeeded with installer %s" %
-                         (tool_class.__name__, t.installed_method.__class__.__name__))
-            else:
-                print(tool_class.__name__)
-                print(t.installed_method.__class__.__name__)
+            t.install()
+            self.assertTrue(t.is_installed(), "installation of tool %s failed" % tool_class.__name__)
+            log.info(".. installation of %s succeeded with installer %s" %
+                     (tool_class.__name__, t.installed_method.__class__.__name__))
+
+
+
