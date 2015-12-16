a3b98c2f4a6d8d1cd634b3a623e7d9ed8af91036
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-16 13:03:53 -0500

    TestToolsInstallation now uses nose-compatible test generators
    test.unit.test_tools.TestToolsInstallation now uses nose-compatible
    test generators. These generators are incompatible with the build-in
    unittest module, so nose must be used for testing

--- travis/install-pip.sh
+++ travis/install-pip.sh
@@ -11,5 +11,5 @@ if [ "$PYVER" = "3" ]; then
     pip install -r requirements-pipes.txt
 fi
 
-echo "pip installing coveralls"
-pip install -q coveralls nose-cov
+echo "pip installing test-related packages (coveralls, etc.)"
+pip install -r requirements-tests.txt
