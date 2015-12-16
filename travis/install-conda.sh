e20865921c90ca80a278a3a0abfdb7ecb3b88e89
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-16 13:57:39 -0500

    changed position of conda dir in PATH

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -9,7 +9,7 @@ fi
 
 bash miniconda.sh -b -p $HOME/miniconda
 chown -R $USER $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+export PATH="$PATH:$HOME/miniconda/bin"
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels bioconda
