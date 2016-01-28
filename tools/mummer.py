05913c8808c793ebc4a8c56267a9472354bce8a3
Danny Park <dpark@broadinstitute.org>
2016-01-28 17:42:14 -0500

    some work on scaffolding w/mummer

--- tools/mummer.py
+++ tools/mummer.py
@@ -6,6 +6,7 @@
 import logging
 import tools
 import util.file
+import util.misc
 import os
 import os.path
 import subprocess
@@ -176,8 +177,9 @@ class MummerTool(tools.Tool):
         os.unlink(tiling)
         
     def align_one_to_one(self, refFasta, otherFasta, outFasta):
-        ''' Take a pseudomolecule (with N's and ragged ends) and produce
-            an aligned-fasta file against a reference genome
+        ''' Take two sequences, align with nucmer, and produce
+            an aligned-fasta file of the two based on show-aligns
+            output.
         '''
         # grab seq_ids (very inefficient, but whatever)
         ref_id = Bio.SeqIO.read(refFasta, 'fasta').id
@@ -213,6 +215,8 @@ class MummerTool(tools.Tool):
 
 
 class AlignsReader(object):
+    ''' This class assists in the parsing and reading of show-aligns output.
+    '''
     def __init__(self, aligns_file, ref_fasta=None):
         self.aligns_file = aligns_file
         self.ref_fasta = ref_fasta
@@ -317,5 +321,63 @@ class AlignsReader(object):
         return str(self.reference_seq.seq[start-1:stop])
 
         
-        
+class FeatureSorter(object):
+    ''' This class helps sort genomic features. It's not terribly optimized
+        for speed or anything. Slightly inspired by calhoun's MultiSequenceRangeMap.
+    '''
+    def __init__(self, collection=None):
+        self.seqids = []
+        self.seq_to_features = {}
+        self.seq_to_breakpoints = {}
+        self.dirty = False
+        if collection is not None:
+            for c, start, stop, strand, other in collection:
+                self.add(c, start, stop, strand, other)
+    
+    def add(self, c, start, stop, strand='+', other=None):
+        if c not in self.seq_to_features:
+            self.seqids.append(c)
+            self.seq_to_features[c] = []
+            self.seq_to_breakpoints[c] = set()
+            #self.seq_to_breakpoints[c].add(1) # do we want the empty interval in front?
+        self.seq_to_features[c].append((int(start), int(stop), strand, other))
+        self.seq_to_breakpoints[c].add(start)
+        self.seq_to_breakpoints[c].add(stop+1)
+        self.dirty = True
+    
+    def _cleanup(self):
+        if self.dirty:
+            self.dirty = False
+            for c in self.seqids:
+                self.seq_to_features[c].sort()
+    
+    def get_features(self, c=None, left=0, right=float('inf')):
+        ''' Get all features on all chromosomes in sorted order. Chromosomes
+            are emitted in order of first appearance (via add). Features on
+            each chromosome are emitted in start, then stop order.  If
+            boundaries are specified, we restrict to features that contain
+            the specified interval.
+        '''
+        self._cleanup()
+        if c is not None:
+            seqlist = [c]
+        else:
+            seqlist = self.seqids
+        for c in self.seqids:
+            for start, stop, strand, other in self.seq_to_features[c]:
+                if stop>=left and start<=right:
+                    yield (c, start, stop, strand, other)
+    
+    def get_intervals(self):
+        ''' Get all intervals on the reference where the overlapping feature
+            set remains the same. Output will be sorted, adjacent intervals
+            and will describe how many and which features overlap it.
+        '''
+        self._cleanup()
+        for c in self.seqids:
+            for left, right in util.misc.pairwise(sorted(self.seq_to_breakpoints[c])):
+                right = right - 1
+                features = list(self.get_features(c, left, right))
+                yield (c, left, right, len(features), features)
+
 
