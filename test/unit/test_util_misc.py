9feca265a2fd2c496bf0fafef5b77315335172c9
Danny Park <dpark@broadinstitute.org>
2016-01-29 11:10:39 -0500

    fixups and unit tests for FeatureSorter and move to util.misc

---
+++ test/unit/test_util_misc.py
@@ -0,0 +1,138 @@
+# Unit tests for util.misc.py
+
+__author__ = "dpark@broadinstitute.org"
+
+import util.misc
+import unittest
+
+
+class TestFeatureSorter(unittest.TestCase):
+
+    def testBasicSortingWithOverlap(self):
+        fs = util.misc.FeatureSorter((
+            ('abca', 10, 20),
+            ('abca', 25, 35),
+            ('abca', 15, 30),
+        ))
+        self.assertEqual(
+            list(fs.get_features()),
+            [
+                ('abca', 10, 20, '+', None),
+                ('abca', 15, 30, '+', None),
+                ('abca', 25, 35, '+', None),
+            ]
+        )
+    
+    def testBasicIntervalsWithOverlap(self):
+        fs = util.misc.FeatureSorter((
+            ('abca', 10, 20),
+            ('abca', 25, 35),
+            ('abca', 15, 30),
+        ))
+        self.assertEqual(
+            list(fs.get_intervals()),
+            [
+                ('abca', 10, 14, 1, [('abca', 10, 20, '+', None),]),
+                ('abca', 15, 20, 2, [('abca', 10, 20, '+', None),('abca', 15, 30, '+', None),]),
+                ('abca', 21, 24, 1, [('abca', 15, 30, '+', None),]),
+                ('abca', 25, 30, 2, [('abca', 15, 30, '+', None),('abca', 25, 35, '+', None),]),
+                ('abca', 31, 35, 1, [('abca', 25, 35, '+', None),]),
+            ]
+        )
+    
+    def testDisjointAndOverlappingIntervals(self):
+        fs = util.misc.FeatureSorter((
+            ('abca', 10, 20),
+            ('abca', 80, 90, '+', None),
+            ('abca', 25, 35, '-'),
+            ('abca', 15, 30),
+        ))
+        self.assertEqual(
+            list(fs.get_intervals()),
+            [
+                ('abca', 10, 14, 1, [('abca', 10, 20, '+', None),]),
+                ('abca', 15, 20, 2, [('abca', 10, 20, '+', None),('abca', 15, 30, '+', None),]),
+                ('abca', 21, 24, 1, [('abca', 15, 30, '+', None),]),
+                ('abca', 25, 30, 2, [('abca', 15, 30, '+', None),('abca', 25, 35, '-', None),]),
+                ('abca', 31, 35, 1, [('abca', 25, 35, '-', None),]),
+                ('abca', 36, 79, 0, []),
+                ('abca', 80, 90, 1, [('abca', 80, 90, '+', None),]),
+            ]
+        )
+    
+    def testMultiChrWindowedFeatures(self):
+        fs = util.misc.FeatureSorter((
+            ('abca', 10, 20),
+            ('aaaa', 17, 33),
+            ('abca', 80, 90),
+            ('abca', 25, 35),
+            ('abca', 15, 30),
+        ))
+        self.assertEqual(
+            list(fs.get_features('abca', 11, 22)),
+            [
+                ('abca', 10, 20, '+', None),
+                ('abca', 15, 30, '+', None),
+            ]
+        )
+
+    def testOpenWindowRight(self):
+        fs = util.misc.FeatureSorter((
+            ('abca', 10, 20),
+            ('aaaa', 17, 33),
+            ('abca', 80, 90),
+            ('abca', 25, 35),
+            ('abca', 15, 30),
+        ))
+        self.assertEqual(
+            list(fs.get_features('abca', 22)),
+            [
+                ('abca', 15, 30, '+', None),
+                ('abca', 25, 35, '+', None),
+                ('abca', 80, 90, '+', None),
+            ]
+        )
+
+    def testOpenWindowLeft(self):
+        fs = util.misc.FeatureSorter((
+            ('abca', 10, 20),
+            ('aaaa', 17, 33),
+            ('abca', 80, 90),
+            ('abca', 25, 35),
+            ('abca', 15, 30),
+        ))
+        self.assertEqual(
+            list(fs.get_features('abca', right=18)),
+            [
+                ('abca', 10, 20, '+', None),
+                ('abca', 15, 30, '+', None),
+            ]
+        )
+    
+    def testMultiChrWithPayloadIntervals(self):
+        fs = util.misc.FeatureSorter((
+            ('abca', 10, 20),
+            ('aaaa', 17, 33, '-', [100, 'name', []]),
+            ('abca', 80, 90, '+', ['other info']),
+            ('abca', 25, 35, '-'),
+            ('abca', 15, 30),
+        ))
+        self.assertEqual(
+            list(fs.get_intervals()),
+            [
+                ('abca', 10, 14, 1, [('abca', 10, 20, '+', None),]),
+                ('abca', 15, 20, 2, [('abca', 10, 20, '+', None),('abca', 15, 30, '+', None),]),
+                ('abca', 21, 24, 1, [('abca', 15, 30, '+', None),]),
+                ('abca', 25, 30, 2, [('abca', 15, 30, '+', None),('abca', 25, 35, '-', None),]),
+                ('abca', 31, 35, 1, [('abca', 25, 35, '-', None),]),
+                ('abca', 36, 79, 0, []),
+                ('abca', 80, 90, 1, [('abca', 80, 90, '+', ['other info']),]),
+                ('aaaa', 17, 33, 1, [('aaaa', 17, 33, '-', [100, 'name', []]),]),
+            ]
+        )
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
--- util/misc.py
+++ util/misc.py
@@ -139,3 +139,66 @@ def run_and_print(args, stdin=None, shell=False, env=None, cwd=None,
     if not silent:
         print(result.stdout.decode('utf-8'))
     return result
+
+class FeatureSorter(object):
+    ''' This class helps sort genomic features. It's not terribly optimized
+        for speed or anything. Slightly inspired by calhoun's MultiSequenceRangeMap.
+    '''
+    def __init__(self, collection=None):
+        self.seqids = []
+        self.seq_to_features = {}
+        self.seq_to_breakpoints = {}
+        self.dirty = False
+        if collection is not None:
+            for args in collection:
+                self.add(*args)
+    
+    def add(self, c, start, stop, strand='+', other=None):
+        ''' Add a "feature", which is a chrom,start,stop,strand tuple (with
+            optional other info attached)
+        '''
+        if c not in self.seq_to_features:
+            self.seqids.append(c)
+            self.seq_to_features[c] = []
+            self.seq_to_breakpoints[c] = set()
+            #self.seq_to_breakpoints[c].add(1) # do we want the empty interval in front?
+        self.seq_to_features[c].append((int(start), int(stop), strand, other))
+        self.seq_to_breakpoints[c].add(start)
+        self.seq_to_breakpoints[c].add(stop+1)
+        self.dirty = True
+    
+    def _cleanup(self):
+        if self.dirty:
+            self.dirty = False
+            for c in self.seqids:
+                self.seq_to_features[c].sort()
+    
+    def get_features(self, c=None, left=0, right=float('inf')):
+        ''' Get all features on all chromosomes in sorted order. Chromosomes
+            are emitted in order of first appearance (via add). Features on
+            each chromosome are emitted in start, then stop order.  If
+            boundaries are specified, we restrict to features that contain
+            the specified interval.
+        '''
+        self._cleanup()
+        if c is not None:
+            seqlist = [c]
+        else:
+            seqlist = self.seqids
+        for c in seqlist:
+            for start, stop, strand, other in self.seq_to_features[c]:
+                if stop>=left and start<=right:
+                    yield (c, start, stop, strand, other)
+    
+    def get_intervals(self):
+        ''' Get all intervals on the reference where the overlapping feature
+            set remains the same. Output will be sorted, adjacent intervals
+            and will describe how many and which features overlap it.
+        '''
+        self._cleanup()
+        for c in self.seqids:
+            for left, right in pairwise(sorted(self.seq_to_breakpoints[c])):
+                right = right - 1
+                features = list(self.get_features(c, left, right))
+                yield (c, left, right, len(features), features)
+
