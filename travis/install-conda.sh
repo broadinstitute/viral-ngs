d1102f68d5adee17afd61b3fdf18fa789e890510
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-03-15 21:03:04 -0400

    also add check for conda executable

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -3,7 +3,7 @@ set -e
 
 # the miniconda directory may exist if it has been restored from cache
 # if it does not exist, we need to install miniconda
-if [ ! -d $MINICONDA_DIR ]; then
+if [ ! -d $MINICONDA_DIR && ! -x "$MINICONDA_DIR/bin/conda"  ]; then
     if [[ "$TRAVIS_PYTHON_VERSION" == 2* ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
     else
@@ -19,4 +19,6 @@ if [ ! -d $MINICONDA_DIR ]; then
     conda config --add channels r
     conda update -q conda
     conda info -a # for debugging
+else
+    echo "Miniconda install already present from cache"
 fi
