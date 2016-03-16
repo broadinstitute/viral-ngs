e1257960b4ca39f9f20bd565f41b03c032a160ff
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-03-15 20:38:57 -0400

    fix muscle for fallback install of unreleased version

--- src/viral_ngs/tools/muscle.py
+++ src/viral_ngs/tools/muscle.py
@@ -15,7 +15,7 @@ TOOL_VERSION = '3.8.1551'
 CONDA_TOOL_VERSION = '3.8.1551'
 TOOL_URL = 'http://www.drive5.com/muscle/downloads{ver}/muscle{ver}_{os}.tar.gz'
 
-UNRELEASED_URL = 'http://www.drive5.com/muscle/muscle_src_3.8.1551.tar.gz'
+UNRELEASED_URL = 'http://www.drive5.com/muscle/muscle_src_{ver}.tar.gz'
 
 _log = logging.getLogger(__name__)
 
@@ -27,14 +27,7 @@ class MuscleTool(tools.Tool):
             install_methods = []
             install_methods.append(tools.CondaPackage(TOOL_NAME, version=CONDA_TOOL_VERSION))
             install_methods.append(
-                tools.DownloadPackage(
-                    UNRELEASED_URL,
-                    'muscle{}/src/muscle'.format(TOOL_VERSION),
-                    post_download_command='cd muscle{}/src; make -s'.format(TOOL_VERSION),
-                    verifycmd='{}/muscle{}/src/muscle -version > /dev/null 2>&1'.format(
-                        util.file.get_build_path(), TOOL_VERSION
-                    )
-                )
+                DownloadAndBuildMuscleUnreleased(ver=TOOL_VERSION)
             )
         tools.Tool.__init__(self, install_methods=install_methods)
 
@@ -74,6 +67,35 @@ class MuscleTool(tools.Tool):
         _log.debug(' '.join(tool_cmd))
         subprocess.check_call(tool_cmd)
     # pylint: enable=W0221
+
+
+class DownloadAndBuildMuscleUnreleased(tools.DownloadPackage):
+
+    def __init__(self, ver):
+        tools.DownloadPackage.__init__(self,
+            UNRELEASED_URL.format(ver=ver, os='src'),
+            'muscle',
+            destination_dir=os.path.join(
+                util.file.get_build_path(),
+                'muscle-{}'.format(ver)),
+            verifycmd='{}/muscle-{}/muscle -version > /dev/null 2>&1'.format(
+                util.file.get_build_path(), TOOL_VERSION),
+            post_download_command='make -s')
+
+    def post_download(self):
+        if os.uname()[0] == 'Darwin':
+            # Makefile needs to be modified for MacOSX
+            badFilePath = os.path.join(self.destination_dir, 'Makefile')
+            os.rename(badFilePath, badFilePath + '.orig')
+            with open(badFilePath, 'wt') as outf:
+                with open(badFilePath + '.orig', 'rt') as inf:
+                    for line in inf:
+                        if line.strip() == 'LDLIBS = -lm -static':
+                            line = '# ' + line
+                        elif line.strip() == '# LDLIBS = -lm':
+                            line = line[2:]
+                        outf.write(line)
+        tools.DownloadPackage.post_download(self)
 
 
 class DownloadAndBuildMuscle(tools.DownloadPackage):
