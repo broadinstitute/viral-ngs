9ffe2930663e08f5921a8160ecd83ed5043b5d59
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-03-15 20:57:19 -0400

    conditionally install conda if its directory is not present
    the miniconda directory may exist if it has been restored from cache.
    if it does not exist, we need to install miniconda

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -e
 
-if [[ "$TRAVIS_PYTHON_VERSION" == 2* ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
-else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-fi
+# the miniconda directory may exist if it has been restored from cache
+# if it does not exist, we need to install miniconda
+if [ ! -d $MINICONDA_DIR ]; then
+    if [[ "$TRAVIS_PYTHON_VERSION" == 2* ]]; then
+        wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
+    else
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    fi
 
-bash miniconda.sh -b -p $HOME/miniconda
-chown -R $USER $HOME/miniconda
-export PATH="$PATH:$HOME/miniconda/bin"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda config --add channels bioconda
-conda config --add channels r
-conda update -q conda
-conda info -a # for debugging
+    bash miniconda.sh -b -p $MINICONDA_DIR
+    chown -R $USER $MINICONDA_DIR
+    export PATH="$PATH:$MINICONDA_DIR/bin"
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    conda config --add channels bioconda
+    conda config --add channels r
+    conda update -q conda
+    conda info -a # for debugging
+fi
