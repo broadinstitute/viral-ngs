77226b08702dacbbcc6b5d9ee6272037a0aa7316
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-03-16 14:07:19 -0400

    have nosetests clear existing logging handlers for travis
    in order for nose to capture log messages, it is necessary to clear
    existing logging handlers via “—logging-clear-handlers” in the
    nosetests call. This should clean up the Travis logs

--- travis/tests-unit.sh
+++ travis/tests-unit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -e
+#set -e
 
-nosetests -v --with-xunit --with-coverage \
+nosetests -v \
+    --logging-clear-handlers \
+    --with-xunit --with-coverage \
     --cover-inclusive --cover-branches --cover-tests \
     --cover-package broad_utils,illumina,assembly,interhost,intrahost,ncbi,read_utils,reports,taxon_filter,tools,util \
     -w test/unit/
