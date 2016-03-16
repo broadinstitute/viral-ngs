21b8e19988f2e83edaf000d49eae8ddc4caedc0f
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-03-16 15:21:19 -0400

    added nose-timer to test dependencies; enabled for travis tests
    nose-timer has been added to report the execution time of each unit test

--- travis/tests-long.sh
+++ travis/tests-long.sh
@@ -8,6 +8,7 @@ if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH = "master" -o -n "$TRAVIS
     echo "This is on master or is a pull request: executing long running tests..."
     nosetests -v \
         --logging-clear-handlers \
+        --with-timer \
         --with-xunit --with-coverage \
         --cover-inclusive --cover-branches --cover-tests \
         --cover-package broad_utils,illumina,assembly,interhost,intrahost,ncbi,read_utils,reports,taxon_filter,tools,util \
