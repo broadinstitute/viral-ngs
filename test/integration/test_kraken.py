2f569918719376a086dff84567081d1759177199
Simon Ye <mail@yesimon.com>
2016-04-22 22:21:41 -0400

    Add integration test for kraken viral mix

--- test/integration/test_kraken.py
+++ test/integration/test_kraken.py
@@ -13,29 +13,28 @@ from test import TestCaseWithTmp
 from test.integration.snake import SnakemakeRunner
 
 
-class TestKrakenTiny(TestCaseWithTmp):
-
-    def setUp(self):
-        super().setUp()
+class TestKrakenBase(TestCaseWithTmp):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.data_dir = join(util.file.get_test_input_path(), 'TestToolKraken')
-        cls.db_dir = os.path.join(cls.data_dir, 'db')
         cls.kraken = tools.kraken.Kraken()
         cls.kraken.install()
-        cls.db = cls.build_kraken_db()
-        cls.inputs = [os.path.join(cls.data_dir, f)
-                      for f in ['zaire_ebola.1.fastq', 'zaire_ebola.2.fastq']]
-        cls.bam = cls.input_bam()
 
     @classmethod
-    def input_bam(cls):
+    def input_bam(cls, sample_name):
         bam = util.file.mkstempfname('.bam')
         picard = tools.picard.FastqToSamTool()
-        picard.execute(cls.inputs[0], cls.inputs[1], 'zaire_ebola', bam)
+        picard.execute(cls.fastqs[0], cls.fastqs[1], 'zaire_ebola', bam)
         return bam
+
+    @classmethod
+    def input_fastqs(cls):
+        fastq1 = util.file.mkstempfname('.1.fastq')
+        fastq2 = util.file.mkstempfname('.2.fastq')
+        picard = tools.picard.SamToFastqTool()
+        picard.execute(cls.bam, fastq1, fastq2)
+        return fastq1, fastq2
 
     @classmethod
     def build_kraken_db(cls):
@@ -52,6 +51,9 @@ class TestKrakenTiny(TestCaseWithTmp):
             '--build': None,
         }).returncode == 0
         return db
+
+
+class CommonTests(object):
 
     @unittest.skipIf(sys.version_info < (3,2), "Python version is too old for snakemake.")
     def test_pipes(self):
@@ -76,11 +78,24 @@ class TestKrakenTiny(TestCaseWithTmp):
         cmd = [bin, 'kraken', self.bam, self.db, '--outReport', out_report, '--outReads', out_reads]
         util.misc.run_and_print(cmd, check=True)
 
+
+class TestKrakenTiny(TestKrakenBase, CommonTests):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.data_dir = join(util.file.get_test_input_path(), 'TestToolKraken')
+        cls.db_dir = os.path.join(cls.data_dir, 'db')
+        cls.db = cls.build_kraken_db()
+        cls.fastqs = [os.path.join(cls.data_dir, f)
+                      for f in ['zaire_ebola.1.fastq', 'zaire_ebola.2.fastq']]
+        cls.bam = cls.input_bam('zaire_ebola')
+
     def test_kraken_tool(self):
         output = os.path.join(tempfile.tempdir, 'zaire_ebola.kraken')
         output_filtered = os.path.join(tempfile.tempdir, 'zaire_ebola.filtered-kraken')
         output_report = os.path.join(tempfile.tempdir, 'zaire_ebola.kraken-report')
-        self.assertEqual(0, self.kraken.classify(self.db, self.inputs, output).returncode)
+        self.assertEqual(0, self.kraken.classify(self.db, self.fastqs, output).returncode)
         result = self.kraken.execute(
             'kraken-filter', self.db, output_filtered, [output],
             options={'--threshold': 0.05})
@@ -88,6 +103,18 @@ class TestKrakenTiny(TestCaseWithTmp):
         result = self.kraken.execute(
             'kraken-report', self.db, output_report, [output_filtered])
         self.assertEqual(0, result.returncode)
+
+
+class TestKrakenVirusMix(TestKrakenBase, CommonTests):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.data_dir = join(util.file.get_test_input_path(), 'TestKrakenViral10')
+        cls.db_dir = os.path.join(cls.data_dir, 'db')
+        cls.db = cls.build_kraken_db()
+        cls.bam = join(cls.data_dir, 'test-reads.bam')
+        cls.fastqs = cls.input_fastqs()
 
 
 if __name__ == '__main__':
