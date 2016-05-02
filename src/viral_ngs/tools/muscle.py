8dac54ffd7f3a70a736db61b133ac55760b6fa78
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-05-02 10:13:11 -0400

    add check=True to run_and_print() calls
    the ‘check’ kwarg (default False) was added to misc.py:run_and_print(),
    which checks the return code of the called command and raises a
    CalledProcessError if returncode != 0. This commit adds check=True to
    instances of run_and_print where run_and_print previously replaced
    invocations of subprocess.check_call() (but not invocations of
    subprocess.call())

--- src/viral_ngs/tools/muscle.py
+++ src/viral_ngs/tools/muscle.py
@@ -68,7 +68,7 @@ class MuscleTool(tools.Tool):
             tool_cmd.extend(('-log', logFile))
 
         _log.debug(' '.join(tool_cmd))
-        util.misc.run_and_print(tool_cmd)
+        util.misc.run_and_print(tool_cmd, check=True)
     # pylint: enable=W0221
 
 
