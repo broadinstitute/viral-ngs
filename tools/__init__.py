6017a446e181b3ca6fbbb76a7a33944d2216b341
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-05-29 01:37:01 -0400

    correct os.environ usage

--- tools/__init__.py
+++ tools/__init__.py
@@ -236,13 +236,13 @@ class CondaPackage(InstallMethod):
         # in newer versions of conda, CONDA_DEFAULT_ENV can be used
         # as it is always the full path (in earlier versions it could be name or path)
         # CONDA_ENV_PATH should be reliable, but may (?) be deprecated in the future 
-        if env_root_path or env or not os.environ["CONDA_ENV_PATH"]:
+        if env_root_path or env or "CONDA_ENV_PATH" not in os.environ or ("CONDA_ENV_PATH" in os.environ and not len(os.environ["CONDA_ENV_PATH"])):
             env_root_path = env_root_path or os.path.join(util.file.get_build_path(), 'conda-tools')
             env = env or 'default'
             self.env_path = os.path.realpath(os.path.expanduser(
                 os.path.join(env_root_path, env)))
         # otherwise, get path of active conda env
-        elif os.environ["CONDA_ENV_PATH"]:
+        elif "CONDA_ENV_PATH" in os.environ and len(os.environ["CONDA_ENV_PATH"]):
             last_path_component = os.path.basename(os.path.normpath(os.environ["CONDA_ENV_PATH"]))
             self.env_path = os.path.dirname(os.environ["CONDA_ENV_PATH"]) if last_path_component == "bin" else os.dirname(os.environ["CONDA_ENV_PATH"])
 
