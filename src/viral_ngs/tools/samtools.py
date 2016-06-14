4529154fcbfddd621eee5462e7719487602982f4
Danny Park <dpark@broadinstitute.org>
2016-06-13 23:25:41 -0400

    create a samtools.isEmpty method to check BAM files in O(1) time

--- src/viral_ngs/tools/samtools.py
+++ src/viral_ngs/tools/samtools.py
@@ -149,3 +149,15 @@ class SamtoolsTool(tools.Tool):
         opts = opts or []
 
         self.execute('mpileup', opts + [inBam], stdout=outPileup, stderr='/dev/null')    # Suppress info messages
+
+    def isEmpty(self, inBam):
+        tmpf = util.file.mkstempfname('.txt')
+        self.dumpHeader(inBam, tmpf)
+        if(os.path.getsize(inBam) > (100 + 5*os.path.getsize(tmpf))):
+            # large BAM file: assume it is not empty
+            # a BAM file definitely has reads in it if its filesize is larger
+            # than just the header itself
+            return False
+        else:
+            # small BAM file: just count and see if it's non-zero
+            return (0==self.count(inBam))
