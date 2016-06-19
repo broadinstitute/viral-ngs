b3df1f021e39e662c5a4403e39ee6aef92ec0dc3
Danny Park <dpark@broadinstitute.org>
2016-06-18 21:10:25 -0400

    remove tools/build from travis cache, remove apt packages for building diamond from scratch

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -3,18 +3,6 @@
 # cache depending on whether we're on the master branch or not.
 set -e
 
-# Only sometimes cache the tools/build directory
-if [ -z "$TRAVIS_TAG" ]; then
-    echo "Travis docker caches allowed for branch $TRAVIS_BRANCH"
-    rm -rf tools/build
-    mkdir -p $CACHE_DIR/tools_build
-    ln -s $CACHE_DIR/tools_build tools/build
-
-else
-    echo "Travis docker cache disabled for tools/build on tag: $TRAVIS_TAG"
-
-fi
-
 # Report how big things are
 echo "Docker cache space usage:"
 du -hs $CACHE_DIR/*
