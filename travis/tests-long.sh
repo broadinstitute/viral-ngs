afb04e7a337195bb04c3cc6171c1a96bf0bc98a9
Danny Park <dpark@broadinstitute.org>
2016-06-21 10:33:12 -0400

    move flake8 to travis/flake8.sh and only run on PRs and master. export PYTEST_ADDOPTS to ensure it gets used for unit and long tests

--- travis/tests-long.sh
+++ travis/tests-long.sh
@@ -6,7 +6,7 @@ echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 
 if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH = "master" -o -n "$TRAVIS_TAG" ]; then
     echo "This is on master or is a pull request: executing long running tests..."
-    PYTEST_ADDOPTS="-rsxX -n 2 --durations=50 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util"
+    export PYTEST_ADDOPTS="-rsxX -n 2 --durations=50 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util"
     py.test --cov-append test/integration
 else
     echo "This is not a pull request: skipping long running tests."
--- travis/tests-unit.sh
+++ travis/tests-unit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #set -e
 
-PYTEST_ADDOPTS="-rsxX -n 2 --durations=50 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util"
+export PYTEST_ADDOPTS="-rsxX -n 2 --durations=50 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util"
 py.test test/unit
