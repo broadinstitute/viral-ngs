62c85b1bcdc336b5659bfa684d5df5d5fe645c46
Danny Park <dpark@broadinstitute.org>
2016-06-23 09:24:59 -0400

    fixes from ct branch

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -8,14 +8,28 @@ else # if it does not exist, we need to install miniconda
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
     
     if [[ "$TRAVIS_PYTHON_VERSION" == 2* ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
-    else
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+        if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+            wget https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh -O miniconda.sh;
+        else
+            wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
+        fi
+     else
+        if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+            wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+        else
+            wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+        fi
     fi
 
     bash miniconda.sh -b -p "$MINICONDA_DIR"
     chown -R "$USER" "$MINICONDA_DIR"
-    export PATH="$PATH:$MINICONDA_DIR/bin"
+    if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+        # on OSX we need to rely on the conda Python rather than the Travis-supplied system Python
+        # so conda has a higher precedence
+        export PATH="$MINICONDA_DIR/bin:$PATH"
+    else
+        export PATH="$PATH:$MINICONDA_DIR/bin"
+    fi
     hash -r
     conda config --set always_yes yes --set changeps1 no
     conda config --add channels bioconda
