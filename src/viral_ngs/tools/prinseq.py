afcf4c0cdee891af4a15a6f32ab0695e98989360
Danny Park <dpark@broadinstitute.org>
2016-07-01 15:25:11 -0400

    cleanup and improvement of prinseq invocations, particularly with paired end reads

--- src/viral_ngs/tools/prinseq.py
+++ src/viral_ngs/tools/prinseq.py
@@ -5,6 +5,7 @@ import os.path
 import shutil
 import subprocess
 import tools
+import util.file
 
 TOOL_NAME = "prinseq"
 TOOL_VERSION = '0.20.4'
@@ -20,38 +21,49 @@ class PrinseqTool(tools.Tool):
         tools.Tool.__init__(self, install_methods=install_methods)
 
     def rmdup_fastq_single(inFastq, outFastq):
-	    # remove duplicate reads and reads with multiple Ns
-	    if not outFastq.endswith('.fastq'):
-	        raise Exception()
-	    if os.path.getsize(inFastq) == 0:
-	        # prinseq-lite fails on empty file input (which can happen in real life
-	        # if no reads match the refDbs) so handle this scenario specially
-	        log.info("output is empty: no reads in input match refDb")
-	        shutil.copyfile(inFastq, outFastq)
-	    else:
-	        cmd = [
-	            'perl', self.install_and_get_path(), '-ns_max_n', '1', '-derep', '1', '-fastq',
-	            inFastq, '-out_bad', 'null', '-line_width', '0', '-out_good', outFastq[:-6]
-	        ]
-	        log.debug(' '.join(cmd))
-	        subprocess.check_call(cmd)
+        ''' remove duplicate reads and reads with multiple Ns '''
 
-    def rmdup_fastq_paired(inFastq1, inFastq2, outFastq1, outFastq2):
-        self.rmdup_fastq_single(inFastq1, outFastq1)
-        self.rmdup_fastq_single(inFastq2, outFastq2)
-        return
-	    # remove duplicate reads and reads with multiple Ns
-	    if not outFastq.endswith('.fastq'):
-	        raise Exception()
-	    if os.path.getsize(inFastq) == 0:
-	        # prinseq-lite fails on empty file input (which can happen in real life
-	        # if no reads match the refDbs) so handle this scenario specially
-	        log.info("output is empty: no reads in input match refDb")
-	        shutil.copyfile(inFastq, outFastq)
-	    else:
-	        cmd = [
-	            'perl', self.install_and_get_path(), '-ns_max_n', '1', '-derep', '1', '-fastq',
-	            inFastq, '-out_bad', 'null', '-line_width', '0', '-out_good', outFastq[:-6]
-	        ]
-	        log.debug(' '.join(cmd))
-	        subprocess.check_call(cmd)
+        outprefix = util.file.mkstempfname("-prinseq-rmdup-out")
+        if os.path.getsize(inFastq) == 0:
+            # prinseq-lite fails on empty file input so handle this scenario specially
+            log.debug("prinseq input is empty")
+            shutil.copyfile(inFastq, outFastq)
+        else:
+            cmd = [
+                'perl', self.install_and_get_path(), '-verbose',
+                '-ns_max_n', '1', '-derep', '1',
+                '-fastq', inFastq,
+                '-out_bad', 'null', '-line_width', '0', '-out_good', outprefix
+            ]
+            log.debug(' '.join(cmd))
+            subprocess.check_call(cmd)
+            shutil.copyfile(outprefix+'.fastq', outFastq)
+            os.unlink(outprefix+'.fastq')
+
+    def rmdup_fastq_paired(inFastq1, inFastq2, outFastq1, outFastq2, purgeUnmated=False):
+        ''' remove duplicate reads and reads with multiple Ns '''
+
+        outprefix = util.file.mkstempfname("-prinseq-rmdup-out")
+        if os.path.getsize(inFastq) == 0:
+            # prinseq-lite fails on empty file input so handle this scenario specially
+            log.debug("prinseq input is empty")
+            shutil.copyfile(inFastq, outFastq)
+        else:
+            cmd = [
+                'perl', self.install_and_get_path(), '-verbose',
+                '-ns_max_n', '1', '-derep', '1',
+                '-fastq', inFastq1, '-fastq2', inFastq2,
+                '-out_bad', 'null', '-line_width', '0', '-out_good', outprefix
+            ]
+            log.debug(' '.join(cmd))
+            subprocess.check_call(cmd)
+            if purgeUnmated:
+                shutil.copyfile(outprefix+'_1.fastq', outFastq1)
+                shutil.copyfile(outprefix+'_2.fastq', outFastq2)
+            else:
+                util.file.cat(outFastq1, outprefix+'_1.fastq', outprefix+'_1_singletons.fastq')
+                util.file.cat(outFastq2, outprefix+'_2.fastq', outprefix+'_2_singletons.fastq')
+            os.unlink(outprefix+'_1.fastq')
+            os.unlink(outprefix+'_2.fastq')
+            os.unlink(outprefix+'_1_singletons.fastq')
+            os.unlink(outprefix+'_2_singletons.fastq')
