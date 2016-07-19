72277ddabad0ccef9f98021ba40746454962b596
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-07-19 14:50:27 -0400

    add matplotlib back to requirements, remove from mocked package list
    Since some of our defaults originate from matplotlib, we may actually
    need it

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath('.')))
 
 # -- Mock out the heavyweight pip packages, esp those that require C ----
 import mock
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'pysam', 'Bio', 'Bio.AlignIO', 'Bio.Alphabet',
+MOCK_MODULES = ['numpy', 'scipy', 'pysam', 'Bio', 'Bio.AlignIO', 'Bio.Alphabet',
                 'Bio.Alphabet.IUPAC', 'Bio.SeqIO', 'Bio.Data.IUPACData']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
