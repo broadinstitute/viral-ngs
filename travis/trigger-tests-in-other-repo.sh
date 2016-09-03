0a5fd0b5bc82c563147a552867fe5df86004f432
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-09-02 21:26:17 -0400

    add scripts to (hopefully) trigger builds in the viral-ngs-deploy repo

---
+++ travis/trigger-tests-in-other-repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# if this is on mater or a pull request, but not a tagged release
+if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH = "master" -o -n "$TRAVIS_TAG" ]; then
+    # if this is a tagged release add that information to the dependent build request
+    if [ -n "$TRAVIS_TAG" ]; then
+        ./travis/trigger-travis.sh --script "UPSTREAM_BRANCH=$TRAVIS_BRANCH UPSTREAM_TAG=$TRAVIS_TAG sh travis/tests-long.sh" broadinstitute viral-ngs-deploy $TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO "UPSTREAM_BRANCH=$TRAVIS_BRANCH UPSTREAM_TAG=$TRAVIS_TAG"
+    else
+        ./travis/trigger-travis.sh --script "UPSTREAM_BRANCH=$TRAVIS_BRANCH sh travis/tests-long.sh" broadinstitute viral-ngs-deploy $TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO "UPSTREAM_BRANCH=$TRAVIS_BRANCH"
+    fi
+fi
