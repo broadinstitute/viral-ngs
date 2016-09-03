d3f3d2de1664e9176c004c502f7c5c8d68a9ae49
Christopher Tomkins-Tinch <tomkinsc@gmail.com>
2016-09-03 14:13:47 -0400

    change quoting of remote trigger command

--- travis/trigger-tests-in-other-repo.sh
+++ travis/trigger-tests-in-other-repo.sh
@@ -10,9 +10,9 @@ if [ ! -z "$TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO" ]; then
         echo "This is on master or is a pull request: executing long running tests..."
         # if this is a tagged release add that information to the dependent build request
         if [ -n "$TRAVIS_TAG" ]; then
-            ./travis/trigger-travis.sh --script "\"UPSTREAM_BRANCH=$TRAVIS_BRANCH UPSTREAM_TAG=$TRAVIS_TAG sh travis/tests-long.sh\"" broadinstitute viral-ngs-deploy $TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO "UPSTREAM_BRANCH=$TRAVIS_BRANCH UPSTREAM_TAG=$TRAVIS_TAG"
+            ./travis/trigger-travis.sh --script 'UPSTREAM_BRANCH=$TRAVIS_BRANCH UPSTREAM_TAG=$TRAVIS_TAG sh travis/tests-long.sh' broadinstitute viral-ngs-deploy $TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO "UPSTREAM_BRANCH=$TRAVIS_BRANCH UPSTREAM_TAG=$TRAVIS_TAG"
         else
-            ./travis/trigger-travis.sh --script "\"UPSTREAM_BRANCH=$TRAVIS_BRANCH sh travis/tests-long.sh\"" broadinstitute viral-ngs-deploy $TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO "UPSTREAM_BRANCH=$TRAVIS_BRANCH"
+            ./travis/trigger-travis.sh --script 'UPSTREAM_BRANCH=$TRAVIS_BRANCH sh travis/tests-long.sh' broadinstitute viral-ngs-deploy $TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO "UPSTREAM_BRANCH=$TRAVIS_BRANCH"
         fi
     fi
 else
