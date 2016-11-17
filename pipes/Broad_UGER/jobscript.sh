bdc2b40b8277685064191880be8213038d47b655
Simon Ye <sye737@gmail.com>
2016-11-17 12:07:41 -0500

    Add --wait-submit option to run-pipe (#521)
    * Add --wait-submit option to run-pipe
    
    This results in not submitting all jobs to the cluster scheduler with
    dependencies right away, instead letting snakemake take control of
    submitting jobs. Useful because the errored jobs in Eqw count against
    your quota even though snakemake is unaware of this.
    
    * Use immediate_submit property in workflow
    
    * Upgrade snakemake to 3.9.0

--- pipes/Broad_UGER/jobscript.sh
+++ pipes/Broad_UGER/jobscript.sh
@@ -65,6 +65,7 @@ echo $JOB_ID
 echo "=============================="
 
 {exec_job}
+EXIT_STATUS=$?
 
 # Report resource consumption because it's not reported by default
 echo "------------------------------"
@@ -75,4 +76,13 @@ qstat -j $JOB_ID | grep '^usage'
 # an error code of 100 is needed since UGER only prevents execution of dependent jobs if the preceding
 # job exits with error code 100
 
-cat $1 &>/dev/null && exit 0 || exit 100
+cat $1 &>/dev/null
+if [[ $? -eq 0 ]]; then
+    exit 0
+else
+    if [[ "{workflow.immediate_submit}" -eq "True" ]]; then
+        exit 100
+    else
+        exit $EXIT_STATUS
+    fi
+fi
