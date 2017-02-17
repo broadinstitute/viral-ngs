1619519c2b1279218de9b310bbf1496f2306a80b
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-02-16 22:47:30 -0500

    WIP conda recipe deplo
    dependencies restructured so conda recipe can source dependencies from
    same lists used for testing. added conda recipe template as jinja2
    recipe, with script to render the recipe with package lists, along with
    release tag as version number, and md5 hash from tagged release
    package. conda push is untested, as is branch/tag interaction (since
    this is the initial commit to GitHub). WIP

--- travis/tests-long.sh
+++ travis/tests-long.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+echo "TRAVIS_TAG: $TRAVIS_TAG"
 
 if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH = "master" -o -n "$TRAVIS_TAG" ]; then
     echo "This is on master or is a pull request: executing long running tests..."
