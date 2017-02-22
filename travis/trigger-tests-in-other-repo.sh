dd5907fad1d06035284b4e99d7b6ce580e269862
Chris Tomkins-Tinch <tomkinsc@gmail.com>
2017-02-21 22:18:09 -0500

    conda recipe fix (#591)
    * add conda-build to requirements, skip cleanup prior to deployment
    
    * updated docs
    
    * chmod +x travis trigger scripts, add jinja to before_deploy
    
    * added python debug statements
    
    * added anaconda-client as installed package
    
    * write conde recipe in text mode
    
    * changed secret for anaconda.org access (broad-viral)
    
    * shift to using the conda-forge curl since the one in bioconda appears to be broken
    
    * changed file read mode in recipe rendering script
    
    * tests-long -> tests-unit

--- travis/trigger-tests-in-other-repo.sh
+++ travis/trigger-tests-in-other-repo.sh
@@ -7,9 +7,9 @@ if [ ! -z "$TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO" ]; then
     echo "TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO is defined"
         # if this is a tagged release add that information to the dependent build request
     if [ -n "$TRAVIS_TAG" ]; then
-        ./travis/trigger-travis.sh --script "env UPSTREAM_BRANCH=$TRAVIS_BRANCH UPSTREAM_TAG=$TRAVIS_TAG bash ./travis/tests-long.sh" broadinstitute viral-ngs-deploy $TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO #"UPSTREAM_BRANCH=$TRAVIS_BRANCH UPSTREAM_TAG=$TRAVIS_TAG"
+        ./travis/trigger-travis.sh --script "env UPSTREAM_BRANCH=$TRAVIS_BRANCH UPSTREAM_TAG=$TRAVIS_TAG bash ./travis/tests-unit.sh" broadinstitute viral-ngs-deploy $TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO #"UPSTREAM_BRANCH=$TRAVIS_BRANCH UPSTREAM_TAG=$TRAVIS_TAG"
     else
-        ./travis/trigger-travis.sh --script "env UPSTREAM_BRANCH=$TRAVIS_BRANCH bash ./travis/tests-long.sh" broadinstitute viral-ngs-deploy $TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO #"UPSTREAM_BRANCH=$TRAVIS_BRANCH"
+        ./travis/trigger-travis.sh --script "env UPSTREAM_BRANCH=$TRAVIS_BRANCH bash ./travis/tests-unit.sh" broadinstitute viral-ngs-deploy $TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO #"UPSTREAM_BRANCH=$TRAVIS_BRANCH"
     fi
 else
     echo "TRAVIS_ACCESS_TOKEN_FOR_OTHER_REPO is undefined. Check the secure variable."
