eba471c2650999c4f6e61889fc5543c4616b94b0
Chris Tomkins-Tinch <tomkinsc@gmail.com>
2017-02-21 20:27:07 -0500

    shift to source curl from conda-forge rather than bioconda (#590)
    * add conda-build to requirements, skip cleanup prior to deployment
    
    * updated docs
    
    * chmod +x travis trigger scripts, add jinja to before_deploy
    
    * added python debug statements
    
    * added anaconda-client as installed package
    
    * write conde recipe in text mode
    
    * changed secret for anaconda.org access (broad-viral)
    
    * shift to using the conda-forge curl since the one in bioconda appears to be broken

--- packaging/conda-recipe/render-recipe.py
+++ packaging/conda-recipe/render-recipe.py
@@ -247,14 +247,14 @@ if __name__ == "__main__":
         output_from_parsed_template = template.render(recipe_variables)
 
         # save the rendered output
-        with open(os.path.join(dir_path, output_directory, template_file), "wb") as f:
+        with open(os.path.join(dir_path, output_directory, template_file), "w") as f:
             f.write(output_from_parsed_template)
 
         # populate md5 hashes for any source urls present
         if(template_file.endswith(".yaml")):
             # calculate and add md5 hashes to recipe
             with open(os.path.join(dir_path, output_directory, template_file), "rb") as inf:
-                with open(os.path.join(dir_path, output_directory, template_file+".checksumed"), "wb") as outf:
+                with open(os.path.join(dir_path, output_directory, template_file+".checksumed"), "w") as outf:
                     for line in inf:
                         # if this is an md5 line, don't write it out
                         if line.strip().startswith("md5"):
