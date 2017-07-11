15b1817dfed14c568fee4cc706f2d4adb821faf6
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-07-11 16:23:13 -0400

    Added ability for config files to include other config files.  (#627)
    Added ability for config files to include other config files, and for specifying "legacy names" by which config params can be accessed.  This is to enable us to add and rename/regroup config params without breaking backwards compatibility.  NOTE: when using cluster submission scripts (run-pipe.sh, run-pipe_UGER.sh, run-pipe_LSF.sh), directory parameters must be specified directly in config.yaml , rather than any files it includes; this restriction will be removed in a future pull request.

--- tests/unit/assemble/test_util_misc.py
+++ tests/unit/assemble/test_util_misc.py
@@ -2,9 +2,11 @@
 
 __author__ = "dpark@broadinstitute.org"
 
-import util.misc
+import os, random, collections
 import unittest
 import subprocess
+import util.misc
+import util.file
 
 
 class TestRunAndPrint(unittest.TestCase):
@@ -176,3 +178,72 @@ class TestFeatureSorter(unittest.TestCase):
                 ('aaaa', 17, 33, 1, [('aaaa', 17, 33, '-', [100, 'name', []]),]),
             ]
         )
+
+
+class TestDictFlatten(unittest.TestCase):
+
+    def testDictFlatten(self):
+        z=dict(a=1,b=dict(c=2,e=3))
+        self.assertEqual(util.misc.unflatten_dict(util.misc.flatten_dict(z)), z)
+
+        self.assertEqual(util.misc.flatten_dict({}),{})
+        self.assertEqual(util.misc.flatten_dict({'a':1}),{('a',): 1})
+        self.assertEqual(util.misc.flatten_dict({'a':1,'b':2}),{('a',): 1, ('b',):2})
+        self.assertEqual(util.misc.flatten_dict({'a':1,'b':{'c':2}}),{('a',): 1, ('b','c'):2})
+        self.assertEqual(util.misc.flatten_dict({'a':1,'b':{'c':2,'d':{'e':3}}}),
+                         {('a',): 1, ('b','c'):2, ('b','d','e'):3})
+
+        # test random dicts
+
+        def rand_dict(max_depth=5):
+            z=dict()
+            for i in range(random.randint(1,3)):
+                z[str(random.randint(0,5))] = random.randint(0,100) if max_depth==0 or random.randint(0,1) \
+                                              else rand_dict(max_depth-1)
+            return z
+
+        for randTestNum in range(1000):
+            z = rand_dict()
+            z_flat=util.misc.flatten_dict(z)
+            assert all( isinstance(k,tuple) and isinstance(v,int) for k,v in z_flat.items())
+            z_unflat=util.misc.unflatten_dict(z_flat)
+            z_reflat=util.misc.flatten_dict(z_unflat)
+            
+            self.assertEqual(z_unflat, z)
+            self.assertEqual(z_reflat, z_flat)
+
+class TestConfigIncludes(unittest.TestCase):
+
+    def testConfigIncludes(self):
+
+        def test_fn(f): return os.path.join(util.file.get_test_input_path(), 'TestUtilMisc', f)
+        cfg1 = util.misc.load_config(test_fn('cfg1.yaml'))
+        cfg2 = util.misc.load_config(test_fn('cfg2.yaml'), std_includes=[test_fn('cfg_std.yaml')],
+                                     param_renamings={'std_param_A_old': 'std_param_A_new'})
+        
+        self.assertIn('paramA', cfg2)
+        self.assertEqual(cfg2["env_vars"]["var_A"],1)
+        self.assertEqual(cfg2["env_vars"]["var_B"],3)
+        self.assertEqual(cfg2["env_vars"]["var_C"],4)
+        with self.assertRaises(KeyError): cfg2["env_vars"]["var_E"]
+        with self.assertRaises(KeyError): cfg2["var_A"]
+        self.assertFalse(cfg2["paramZ"])
+        self.assertTrue(cfg1["paramZ"])
+
+        self.assertEqual(cfg2["std_methods"], ['a','b','c'])
+        self.assertEqual(cfg2["stage1"]["stage2"]["step_num"],3)
+        self.assertEqual(cfg2["stage1"]["stage2"]["step_list"],[5,10,15])
+        self.assertEqual(cfg2["stage1"]["stage3"]["step_list"],[3,33])
+        self.assertEqual(cfg1["stage1"]["stage3"]["step_list"],[51,101,151])
+
+        self.assertEqual(cfg2["std_param_A_new"], 111)  # specified as std_param_A_old in cfg1.yaml
+
+        self.assertEqual(util.misc.load_config(test_fn('empty.yaml')), {})
+
+
+
+
+
+        
+
+
