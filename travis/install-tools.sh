146f69d2384af8fd4b9056fe7a56d62749a7a320
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-08-01 17:39:54 -0400

    hide conda install progress bar

--- travis/install-tools.sh
+++ travis/install-tools.sh
@@ -42,7 +42,7 @@ echo "Installing and validating bioinformatic tools"
 export CONDA_ENVS_PATH=tools/conda-cache:tools/conda-tools/default
 
 for i in $(seq 3); do
-  conda create -y -m -c broad-viral -c r -c bioconda -c conda-forge -c defaults -p tools/conda-tools/default --file $HOME/requirements-conda.txt python="$TRAVIS_PYTHON_VERSION" && break
+  conda create -q -y -m -c broad-viral -c r -c bioconda -c conda-forge -c defaults -p tools/conda-tools/default --file $HOME/requirements-conda.txt python="$TRAVIS_PYTHON_VERSION" && break
   sleep 5
 done
 
