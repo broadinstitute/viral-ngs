594923729f40a7ea9c16a3500606a5e66022a50a
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-08-15 19:58:18 -0400

    add pysftp to py3 requirements

--- travis/tests-long.sh
+++ travis/tests-long.sh
@@ -9,7 +9,7 @@ if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH = "master" -o -n "$TRAVIS
     echo "This is on master or is a pull request: executing long running tests..."
     export PYTEST_ADDOPTS="-rsxX -n 2 --durations=50 --fixture-durations=20 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util"
     py.test --cov-append test/integration
-    sleep 2
+    sleep 2 # sleep to allow logs to be printed without truncation in the event of error
 else
     echo "This is not a pull request: skipping long running tests."
 fi
