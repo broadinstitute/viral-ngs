1d00eaf5d8eeb794676e2f2cc85d684cc86728d1
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-08-16 10:47:36 -0400

    "py.test" -> "pytest"

--- travis/tests-unit.sh
+++ travis/tests-unit.sh
@@ -5,7 +5,7 @@ UNIT_TESTS_ALWAYS=1
 
 if [ $UNIT_TESTS_ALWAYS ] || [ $TRAVIS_PULL_REQUEST == "false" ]; then
     export PYTEST_ADDOPTS="-rsxX --durations=50 --fixture-durations=20 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util"
-    py.test test/unit
+    pytest test/unit
     sleep 2
 else
     echo "Skipping unit tests for pull request - see branch tests."
