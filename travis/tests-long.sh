71e7d081d4b26cace28d4d9a083444cc69073fa7
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-08-16 10:01:13 -0400

    let's try omitting pytest-xdist

--- travis/tests-long.sh
+++ travis/tests-long.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#set -e
+set -e
 
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_TAG: $TRAVIS_TAG"
 
-if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH = "master" -o -n "$TRAVIS_TAG" ]; then
+if [[ ( -n $TRAVIS_PULL_REQUEST && $TRAVIS_PULL_REQUEST != "false" ) || $TRAVIS_BRANCH = "master" || -n "$TRAVIS_TAG" ]]; then
     echo "This is on master or is a pull request: executing long running tests..."
-    export PYTEST_ADDOPTS="-rsxX -n 2 --durations=50 --fixture-durations=20 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util"
+    export PYTEST_ADDOPTS="-rsxX --durations=50 --fixture-durations=20 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util"
     py.test --cov-append test/integration
     sleep 2 # sleep to allow logs to be printed without truncation in the event of error
 else
