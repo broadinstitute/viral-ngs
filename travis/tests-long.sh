9008f1cf3f86bbe51b330b9be462811da3191285
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-08-16 22:18:15 -0400

    sleep and exit with error, on error

--- travis/tests-long.sh
+++ travis/tests-long.sh
@@ -9,8 +9,9 @@ if [[ ( -n $TRAVIS_PULL_REQUEST && $TRAVIS_PULL_REQUEST != "false" ) || $TRAVIS_
     echo "This is on master or is a pull request: executing long running tests..."
     # pytest uses "-r[outputLetterCodes]" to set outputs, these are not other arguments composed into a single argument
     export PYTEST_ADDOPTS="-rsxX --exitfirst --durations=50 --fixture-durations=20 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util"
-    pytest --cov-append test/integration || exit $?
-    #sleep 4 # sleep to allow logs to be printed without truncation in the event of error
+    pytest --cov-append test/integration
+    rc=$?; if [[ $rc != 0 ]]; then sleep 4 && exit $rc; fi
+    # sleep to allow logs to be printed without truncation in the event of error
 else
     echo "This is not a pull request: skipping long running tests."
 fi
