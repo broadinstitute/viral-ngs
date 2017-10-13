789be384c7a8168ac466d1868894fd4923d97107
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-10-13 17:19:49 -0400

    render+build the conda package on PRs, on master, and on tags, but only upload to anaconda.org on tags

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -37,16 +37,26 @@ PKG_VERSION=$1
 echo "Python binary: $(which python)"
 echo "Python version: $(python --version)"
 
-# if the ANACONDA_TOKEN is defined (not on an external branch)
-if [ ! -z "$ANACONDA_TOKEN" ]; then
-    echo "Running $SCRIPTPATH/package-conda.sh"
+# If this is a PR, on the master branch, or is a tag, render and build the conda package. If it is a tag, also upload to anaconda.org
+if [[ ( -n $TRAVIS_PULL_REQUEST && $TRAVIS_PULL_REQUEST != "false" ) || $TRAVIS_BRANCH = "master" || -n "$TRAVIS_TAG" ]]; then
+    echo "Rendering and building conda package..."
     # Render recipe from template and dependency files, setting the tag as the current version
-    conda config --set anaconda_upload yes
-    python packaging/conda-recipe/render-recipe.py "$PKG_VERSION" --build-reqs requirements-conda.txt --run-reqs requirements-conda.txt --py3-run-reqs requirements-py3.txt --py2-run-reqs requirements-py2.txt --test-reqs requirements-conda-tests.txt && \
-        CONDA_PERL=5.22.0 conda build -c broad-viral -c r -c bioconda -c conda-forge -c defaults --python "$TRAVIS_PYTHON_VERSION" --token "$ANACONDA_TOKEN" packaging/conda-recipe/viral-ngs && \
-        ./travis/trigger-tests-in-other-repo.sh
-        # check the exit code of conda build, and if successful,
-        # trigger the viral-ngs-deploy repository to test/build the docker container
-else
-    echo "ANACONDA_TOKEN is not defined. Conda package upload is only supported for branches on the original repository."
-fi
+    # if this is a tag build+upload, otherwise just test building
+    if [ -n "$TRAVIS_TAG" ]; then
+         # if the ANACONDA_TOKEN is defined (not on an external branch)
+        if [ ! -z "$ANACONDA_TOKEN" ]; then
+            conda config --set anaconda_upload yes
+            python packaging/conda-recipe/render-recipe.py "$PKG_VERSION" --build-reqs requirements-conda.txt --run-reqs requirements-conda.txt --py3-run-reqs requirements-py3.txt --py2-run-reqs requirements-py2.txt --test-reqs requirements-conda-tests.txt && \
+            CONDA_PERL=5.22.0 conda build -c broad-viral -c r -c bioconda -c conda-forge -c defaults --python "$TRAVIS_PYTHON_VERSION" --token "$ANACONDA_TOKEN" packaging/conda-recipe/viral-ngs && \
+            ./travis/trigger-tests-in-other-repo.sh
+            # check the exit code of conda build, and if successful,
+            # trigger the viral-ngs-deploy repository to test/build the docker container
+        else
+            echo "ANACONDA_TOKEN is not defined. Conda package upload is only supported for branches on the original repository."
+        fi
+    else
+        python packaging/conda-recipe/render-recipe.py "`git rev-parse --short HEAD`" --build-reqs requirements-conda.txt --run-reqs requirements-conda.txt --py3-run-reqs requirements-py3.txt --py2-run-reqs requirements-py2.txt --test-reqs requirements-conda-tests.txt && \
+        CONDA_PERL=5.22.0 conda build -c broad-viral -c r -c bioconda -c conda-forge -c defaults --python "$TRAVIS_PYTHON_VERSION" --no-anaconda-upload packaging/conda-recipe/viral-ngs
+    fi
+fi  
+
