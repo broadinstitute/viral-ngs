2f4d866178ecc4763ed448882dd18bdc6cb16797
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-10-23 15:51:10 -0400

    suffix correction

--- travis/build-package.sh
+++ travis/build-package.sh
@@ -31,7 +31,7 @@ SCRIPTPATH="$(cd -P "$SCRIPT_DIRNAME" &> /dev/null && pwd)"
 SCRIPT="$SCRIPTPATH/$(basename "$SCRIPT")"
 
 PKG_VERSION=$1
-CONDA_PACKAGE_OUTDIR=packacking/conda-packages/
+CONDA_PACKAGE_OUTDIR=packacking/conda-packages
 
 # === Build conda package
 
@@ -80,7 +80,7 @@ if [ $BUILD_PACKAGE = "true" ]; then
             # build the conda package
             described_version="$(./assembly.py --version)"
             package_suffix="+$described_version"
-            python packaging/conda-recipe/render-recipe.py "0.0.0+$package_suffix" --package-name "viral-ngs-dev" --download-filename "$TRAVIS_BRANCH" --build-reqs requirements-conda.txt --run-reqs requirements-conda.txt --py3-run-reqs requirements-py3.txt --py2-run-reqs requirements-py2.txt --test-reqs requirements-conda-tests.txt && \
+            python packaging/conda-recipe/render-recipe.py "0.0.0$package_suffix" --package-name "viral-ngs-dev" --download-filename "$TRAVIS_BRANCH" --build-reqs requirements-conda.txt --run-reqs requirements-conda.txt --py3-run-reqs requirements-py3.txt --py2-run-reqs requirements-py2.txt --test-reqs requirements-conda-tests.txt && \
             #CONDA_PERL=5.22.0 conda build -c broad-viral -c r -c bioconda -c conda-forge -c defaults --python "$TRAVIS_PYTHON_VERSION" --no-anaconda-upload --output-folder "$CONDA_PACKAGE_OUTDIR" packaging/conda-recipe/viral-ngs
             CONDA_PERL=5.22.0 conda build -c broad-viral -c r -c bioconda -c conda-forge -c defaults --python "$TRAVIS_PYTHON_VERSION" --output-folder "$CONDA_PACKAGE_OUTDIR" packaging/conda-recipe/viral-ngs
             cp $CONDA_PACKAGE_OUTDIR/*.tar.bz2 ./docker/
