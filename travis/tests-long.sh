8a8afeb9dc0ef76a51f6b1e807d4e3c3a58156db
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-10-23 16:04:01 -0400

    strcmp change

--- travis/tests-long.sh
+++ travis/tests-long.sh
@@ -5,7 +5,7 @@ echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_TAG: $TRAVIS_TAG"
 
-if [ $PERFORM_TESTING = "true" ]; then
+if [[ "$PERFORM_TESTING" = "true" ]]; then
     if [[ ( -n $TRAVIS_PULL_REQUEST && $TRAVIS_PULL_REQUEST != "false" ) || $TRAVIS_BRANCH = "master" || -n "$TRAVIS_TAG" ]]; then
         echo "This is on master or is a pull request: executing long running tests..."
         export PYTEST_ADDOPTS="-rsxX -n 2 --durations=20 --fixture-durations=20 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util --basetemp /dev/shm/test"
--- travis/tests-unit.sh
+++ travis/tests-unit.sh
@@ -2,7 +2,7 @@
 
 UNIT_TESTS_ALWAYS=1
 
-if [ $PERFORM_TESTING = "true" ]; then
+if [[ "$PERFORM_TESTING" = "true" ]]; then
     if [ $UNIT_TESTS_ALWAYS ] || [ $TRAVIS_PULL_REQUEST = "false" ]; then
         export PYTEST_ADDOPTS="-rsxX -n 2 --durations=50 --fixture-durations=20 --junit-xml=pytest.xml --cov-report= --cov broad_utils --cov illumina --cov assembly --cov interhost --cov intrahost --cov metagenomics --cov ncbi --cov read_utils --cov reports --cov taxon_filter --cov tools --cov util --basetemp /dev/shm/test"
 
