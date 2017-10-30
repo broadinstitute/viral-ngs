6e1faa030981a9f5f12d839642fdc68a88088bbc
Danny Park <dpark@broadinstitute.org>
2017-10-30 16:14:30 -0400

    try to get it to stop cloning fresh

--- docker/install-skeleton.sh
+++ docker/install-skeleton.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 
 mkdir -p /opt/viral-ngs/viral-ngs-etc
 ln -s /opt/viral-ngs/source /opt/viral-ngs/viral-ngs-etc/viral-ngs
-ln -s /opt/docker/easy-deploy-viral-ngs.sh /opt/viral-ngs
-ln -s /opt/docker/env_wrapper.sh /opt/viral-ngs
+ln /opt/docker/easy-deploy-viral-ngs.sh /opt/viral-ngs
+ln /opt/docker/env_wrapper.sh /opt/viral-ngs
 
-/opt/docker/easy-deploy-viral-ngs.sh setup-git-local
+/opt/viral-ngs/easy-deploy-viral-ngs.sh setup-git-local
