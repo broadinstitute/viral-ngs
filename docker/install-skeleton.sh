f3592e4e438ab880b8834d89b8ffc16949f35282
Danny Park <dpark@broadinstitute.org>
2017-10-30 22:11:50 -0400

    fix invocation of docker build, break out requirements for conda buliding into its own file

--- docker/install-skeleton.sh
+++ docker/install-skeleton.sh
@@ -4,6 +4,8 @@ set -e -o pipefail
 
 mkdir -p /opt/viral-ngs/viral-ngs-etc
 ln -s /opt/viral-ngs/source /opt/viral-ngs/viral-ngs-etc/viral-ngs
+ln -s /opt/miniconda /opt/viral-ngs/viral-ngs-etc/conda-env
+export VIRAL_CONDA_ENV_PATH=/opt/miniconda
 ln /opt/docker/easy-deploy-viral-ngs.sh /opt/viral-ngs
 ln /opt/docker/env_wrapper.sh /opt/viral-ngs
 
