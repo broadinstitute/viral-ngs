78fc36bcc89aa2b9e6e93e203fdf2f409da0b112
Danny Park <dpark@broadinstitute.org>
2017-11-20 13:37:39 -0500

    add cromwell-on-local test (just one for now)

--- travis/tests-cromwell.sh
+++ travis/tests-cromwell.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e -o pipefail
 
-echo "TO DO: make minimal pipeline tests"
+ln -s pipes/WDL/workflows pipes/WDL/workflows/tasks .
 
-#for workflow in pipes/WDL/workflows/*.wdl; do
-#  java -jar cromwell-29.jar run $workflow -i pipes/WDL/test_inputs/somesortoftestminimaldata.json
-#done
+for workflow in pipes/WDL/workflows/*.wdl; do
+	workflow_name=$(basename $workflow .wdl)
+	input_json="test/input/WDL/test_values-$workflow_name-local.json"
+	if [ -f $input_json ]; then
+		java -jar cromwell-29.jar run workflows/$workflow_name.wdl -i $input_json
 
+		echo "note: there is no testing of output yet..."
+    fi
+done
