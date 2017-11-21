5159176836e7ed33c5e16b864c68f95323091c8b
Danny Park <dpark@broadinstitute.org>
2017-11-20 22:03:29 -0500

    fix conda build, silence coveralls from non-docker py36 build

--- travis/install-conda-build.sh
+++ travis/install-conda-build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-set -e
-
-conda install -q -y conda-build==3.0.25
-conda install -q -y anaconda-client
-conda create -m -q -y -p tools/conda-tools/default Jinja2==2.8
+conda install -q -y conda-build==3.0.25 anaconda-client Jinja2==2.8
