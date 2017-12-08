143c09c7887860bc5d9dcf644158c1a68bea6a23
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-12-08 16:39:46 -0500

    cruft removal, docker login to mirror only if credentials are populated

--- travis/deploy-docker.sh
+++ travis/deploy-docker.sh
@@ -6,7 +6,10 @@ if [[ -n "$DOCKER_PASS" && -n "$DOCKER_USER" && -n "$DOCKER_REGISTRY" ]]; then
 
 	# log in to Docker registry
 	echo "$DOCKER_PASS" | docker login --password-stdin --username "$DOCKER_USER" "$DOCKER_REGISTRY"
-	echo "$DOCKER_PASS_MIRROR" | docker login --password-stdin --username "$DOCKER_USER_MIRROR" "$DOCKER_REGISTRY_MIRROR"
+
+	if [ -n "$DOCKER_USER_MIRROR" -a -n "$DOCKER_PASS_MIRROR" -a -n "$DOCKER_REGISTRY_MIRROR" ]; then
+		echo "$DOCKER_PASS_MIRROR" | docker login --password-stdin --username "$DOCKER_USER_MIRROR" "$DOCKER_REGISTRY_MIRROR"
+	fi
 
 	# tag and deploy
 	for TAG in `travis/list-docker-tags.sh`; do
