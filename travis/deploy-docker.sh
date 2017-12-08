801d1f807ed974732f7e4f2df88f34473ba6adbe
Chris Tomkins-Tinch <tomkinsc@gmail.com>
2017-12-08 17:11:00 -0500

    also login and push to mirror docker registry (docker.io) (#735)
    * also login and push to mirror docker registry (docker.io)
    
    * username is a username and not an email
    
    * cruft removal, docker login to mirror only if credentials are populated

--- travis/deploy-docker.sh
+++ travis/deploy-docker.sh
@@ -7,6 +7,10 @@ if [[ -n "$DOCKER_PASS" && -n "$DOCKER_USER" && -n "$DOCKER_REGISTRY" ]]; then
 	# log in to Docker registry
 	echo "$DOCKER_PASS" | docker login --password-stdin --username "$DOCKER_USER" "$DOCKER_REGISTRY"
 
+	if [ -n "$DOCKER_USER_MIRROR" -a -n "$DOCKER_PASS_MIRROR" -a -n "$DOCKER_REGISTRY_MIRROR" ]; then
+		echo "$DOCKER_PASS_MIRROR" | docker login --password-stdin --username "$DOCKER_USER_MIRROR" "$DOCKER_REGISTRY_MIRROR"
+	fi
+
 	# tag and deploy
 	for TAG in `travis/list-docker-tags.sh`; do
 		echo "Pushing docker image to docker registry as $TAG"
