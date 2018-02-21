5a1646538046fd38fccbd53467fdbdda1f03cbe9
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-02-21 15:18:52 -0500

    refactor tests

--- test/unit/test_util_metadata.py
+++ test/unit/test_util_metadata.py
@@ -15,7 +15,7 @@ import util.file
 import util.misc
 from util.metadata import InFile, OutFile
 from util._metadata import metadata_db, file_arg, md_utils
-from test import tst_inp
+from test import tst_inp, TestCaseWithTmp
 
 from test.unit import tst_cmds
 
@@ -28,41 +28,34 @@ def tmp_metadata_db(tmpdir):
     with util.misc.tmp_set_env('VIRAL_NGS_METADATA_PATH', metadata_db_path):
         yield metadata_db_path
 
-def test_metadata_recording(tmp_metadata_db):
-    """Test basic metadata recording"""
-    with util.file.tempfname('.txt') as size_fname:
-        data1_fname = tst_inp('TestUtilCmd/data1.txt')
-        util.cmd.run_cmd(tst_cmds, 'get_file_size', [data1_fname, size_fname])
-        assert util.file.slurp_file(size_fname) == str(os.path.getsize(data1_fname))
-        records = metadata_db.load_all_records()
-        assert len(records)==1
-        step_record = records[0]
+@pytest.mark.usefixtures('tmp_metadata_db')
+class TestMetadataRecording(TestCaseWithTmp):
 
-        def canonicalize_step(s):
-            """From a step record, either remove keys whose values change between execution, or canonicalize the values by replacing
-            them with just the value type."""
-            return {k: type(v) if len(k)>1 and k[1] in ('run_env', 'run_info', 'run_id', 'step_id', 'version_info') or \
-                    len(k)>3 and k[1]=='args' and (k[3]=='val' or k[3]=='files' and k[5] not in ('hash', 'size')) \
-                    else v for k, v in util.misc.flatten_dict(s, as_dict=(tuple,list)).items() \
-                    if k[:3] != ('step', 'run_info', 'argv')}
+    def canonicalize_step(self, step_record):
+        """From a step record, either remove keys whose values change between execution, or canonicalize the values by replacing
+        them with just the value type."""
+        return {k: type(v) if len(k)>1 and k[1] in ('run_env', 'run_info', 'run_id', 'step_id', 'version_info') or \
+                len(k)>3 and k[1]=='args' and (k[3]=='val' or k[3]=='files' and k[5] not in ('hash', 'size')) \
+                else v for k, v in util.misc.flatten_dict(step_record, as_dict=(tuple,list)).items() \
+                if k[:3] != ('step', 'run_info', 'argv')}
 
-        def chk_step(s, fn):
-            """Check the step record `s` against expected data from file identified by `fn`"""
-            expected = md_utils.byteify(json.loads(util.file.slurp_file(tst_inp('TestUtilCmd/expected.{}.step.json.gz'.format(fn)))))
-            assert canonicalize_step(s) == canonicalize_step(expected)
+    def chk_step(self, step_record, expected_fname):
+        """Check the step record `step_record` against expected data from file identified by `expected_fname`"""
+        expected_step_record = md_utils.byteify(json.loads(util.file.slurp_file(expected_fname)))
+        assert self.canonicalize_step(step_record) == self.canonicalize_step(expected_step_record)
 
-        chk_step(step_record, 'get_file_size.data1')
+    def test_one_cmd(self):
+        """Test basic metadata recording"""
+        with util.file.tempfname('.txt') as size_fname:
+            data1_fname = self.input('data1.txt')
+            util.cmd.run_cmd(tst_cmds, 'get_file_size', [data1_fname, size_fname])
+            assert util.file.slurp_file(size_fname) == str(os.path.getsize(data1_fname))
+            records = metadata_db.load_all_records()
+            assert len(records)==1
+            step_record = records[0]
 
+            expected_step1 = self.input('expected.get_file_size.data1.step.json.gz')
 
-def test_metadata_recording2(tmp_metadata_db):
-    """Test basic metadata recording"""
-    with util.file.tempfname('.tsv') as metricsFile:
-        import reports
-        util.cmd.run_cmd(reports, 'final_assembly_metrics', [tst_inp('TestAssembleSpades/trinity_contigs.fasta'), metricsFile])
-        records = metadata_db.load_all_records()
-        assert len(records)==1
-        step_record = records[0]
-        assert metadata_db.is_valid_step_record(step_record)
-        assert set(step_record['step']['args'].keys()) == set(('assembly_fname', 'metrics_fname'))
-        assert step_record['step']['cmd_module'] == 'reports'
-        assert step_record['step']['cmd_name'] == 'final_assembly_metrics'
+            self.chk_step(step_record, expected_step1)
+
+# end: class TestMetadataRecording(TestCaseWithTmp)
