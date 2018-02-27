5a0fb1667af7a3a47db562f423db2fc42474d8db
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-02-27 13:56:16 -0500

    added testing of metadata logging to s3

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,12 @@
-
-import shutil
+import os
 
 import util.misc
-import util._metadata.metadata_db
 
 import pytest
 
 @pytest.fixture(scope='session')
 def tmp_metadata_db(tmpdir_factory):
     """Sets up the metadata database in a temp dir"""
-    metadata_db_path = tmpdir_factory.mktemp('metadata_db')
+    metadata_db_path = os.environ.get('VIRAL_NGS_TEST_METADATA_PATH', tmpdir_factory.mktemp('metadata_db'))
     with util.misc.tmp_set_env('VIRAL_NGS_METADATA_PATH', metadata_db_path):
         yield metadata_db_path
