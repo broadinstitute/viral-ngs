8759f05df02404783177c6c29bd6729eca710ede
Danny Park <dpark@broadinstitute.org>
2018-03-09 08:55:47 -0500

    fix validate wdl script

--- travis/validate-wdl.sh
+++ travis/validate-wdl.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 set -e -o pipefail
 
-ln -s pipes/WDL/workflows/tasks .
-for workflow in pipes/WDL/workflows/*.wdl; do
+# validate each imported library of tasks on its own
+for tasks in pipes/WDL/workflows/tasks/*.wdl; do
+  echo "validating tasks $tasks"
+  java -jar womtool.jar validate $tasks
+done
+
+# validate the workflow files
+# unfortunately, dxWDL now requires the -imports parameter and cromwell supports
+# it as well but womtool validate does not yet support it! so we have to copy
+# everything to a temp dir
+mkdir wdl_validate_test
+cd wdl_validate_test
+cp ../pipes/WDL/workflows/tasks/*.wdl ../pipes/WDL/workflows/*.wdl .
+for workflow in ../pipes/WDL/workflows/*.wdl; do
+  workflow=`basename $workflow`
   echo "validating $workflow"
   java -jar womtool.jar validate $workflow
 done
-rm tasks
+cd -
+rm -r wdl_validate_test
