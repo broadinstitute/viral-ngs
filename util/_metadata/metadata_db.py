38d4308ad4a5453c8f937ce7264f02b0fe0d2e0d
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-03-12 22:27:21 -0400

    changed (mostly) to .json.gz

--- util/_metadata/metadata_db.py
+++ util/_metadata/metadata_db.py
@@ -6,6 +6,8 @@ import builtins
 import os
 import functools
 import operator
+import io
+import gzip
 
 import util.misc
 from . import md_utils
@@ -37,8 +39,12 @@ def load_all_records():
     records = []
     with fs.open_fs(metadata_dir().split()[-1]) as metadata_fs:
         for f in sorted(metadata_fs.listdir(u'/')):
-            if f.endswith('.json'):
-                json_str = metadata_fs.gettext(f)
+            if f.endswith('.json.gz'):
+                json_data_gzipped = metadata_fs.getbytes(f)
+                with io.BytesIO(json_data_gzipped) as fgz:
+                    with gzip.GzipFile(mode='rb', fileobj=fgz) as gzip_obj:
+                        json_bytes = gzip_obj.read()
+                json_str = json_bytes.decode()
                 step_record = md_utils.byteify(json.loads(json_str))
                 if is_valid_step_record(step_record):
                     records.append(step_record)
@@ -46,7 +52,7 @@ def load_all_records():
 
 def load_step_record(step_id):
     """Load one step record from the database"""
-    json_fname = u'{}.json'.format(step_id)
+    json_fname = u'{}.json.gz'.format(step_id)
     with fs.open_fs(metadata_dir().split()[-1]) as metadata_fs:
         json_str = metadata_fs.gettext(json_fname)
         step_record = md_utils.byteify(json.loads(json_str))
@@ -55,10 +61,17 @@ def load_step_record(step_id):
 
 def store_step_record(step_data, write_obj):
     """Store step record to metadata database(s)"""
-    json_fname = u'{}.json'.format(step_data['step']['step_id'])
+    json_fname = u'{}.json.gz'.format(step_data['step']['step_id'])
     json_str = json.dumps(step_data, sort_keys=True, indent=4, default=write_obj)
-    if hasattr(builtins, 'unicode'): json_str = unicode(json_str)
+    json_bytes = unicode(json_str) if hasattr(builtins, 'unicode') else json_str.encode('utf-8')
+
+    fgz = io.BytesIO()
+    with gzip.GzipFile(filename=json_fname, mode='wb', fileobj=fgz) as gzip_obj:
+        gzip_obj.write(json_bytes)
+    json_data_gzipped = fgz.getvalue()
+
     for mdir in metadata_dir().split():
         with fs.open_fs(mdir) as metadata_fs:
-            metadata_fs.settext(json_fname, json_str)
+            metadata_fs.setbytes(json_fname, json_data_gzipped)
+
 
