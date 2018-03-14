f32173d61a3cea4660d2ca84e529d174aa0cfe21
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-03-13 22:52:15 -0400

    added back recording to s3

--- test/plugins/metadata_tester.py
+++ test/plugins/metadata_tester.py
@@ -4,6 +4,7 @@ import os
 import os.path
 import functools
 import operator
+import uuid
 
 import util.misc
 import util.file
@@ -31,15 +32,14 @@ def canonicalize_step_records(step_records, canonicalize_keys=()):
                                             [list(canonicalize_step_record(r, canonicalize_keys).items())
                                              for r in step_records], [])))
 
-#@pytest.fixture(scope='session', autouse='true')
+@pytest.fixture(scope='session', autouse='true')
 def tmp_metadata_db(tmpdir_factory):
     """Sets up the metadata database in a temp dir"""
     metadata_db_path = os.environ.get('VIRAL_NGS_TEST_METADATA_PATH', tmpdir_factory.mktemp('metadata_db'))
     with util.misc.tmp_set_env('VIRAL_NGS_METADATA_PATH', metadata_db_path):
         yield metadata_db_path        
 
-
-@pytest.fixture(autouse='true')
+#@pytest.fixture(autouse='true')
 def per_test_metadata_db(request, tmpdir_factory):
     """Sets up the metadata database in a temp dir"""
     metadata_db_path = tmpdir_factory.mktemp('metadata_db')
@@ -92,6 +92,7 @@ def no_detailed_env():
 
 @pytest.fixture(autouse='true')
 def set_nodeid_metadata(request):
-    """Add pytest nodeid to the metadata"""
-    with util.misc.tmp_set_env('VIRAL_NGS_METADATA_VALUE_pytest_nodeid', request.node.nodeid):
+    """Add pytest nodeid and a unique ID of this test invocation to the metadata"""
+    with util.misc.tmp_set_env('VIRAL_NGS_METADATA_VALUE_pytest_nodeid', request.node.nodeid), \
+         util.misc.tmp_set_env('VIRAL_NGS_METADATA_VALUE_pytest_testid', uuid.uuid4()):
         yield
