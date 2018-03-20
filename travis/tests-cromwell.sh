bdf73529025726d811f874ab2c4b7403e3444f38
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-03-20 17:06:04 -0400

    test just the local cromwell

--- travis/tests-cromwell.sh
+++ travis/tests-cromwell.sh
@@ -3,27 +3,24 @@ set -x -e  # intentionally allow for pipe failures below
 
 ln -s $GATK_PATH/GenomeAnalysisTK.jar .
 mkdir -p workflows
-ln -s pipes/WDL/workflows/*.wdl pipes/WDL/workflows/tasks/*.wdl workflows
+cp *.jar test/input/WDL/test_inputs-*-local.json pipes/WDL/workflows/*.wdl pipes/WDL/workflows/tasks/*.wdl workflows
 cd workflows
 
 for workflow in ../pipes/WDL/workflows/*.wdl; do
 	workflow_name=$(basename $workflow .wdl)
-	input_json="../test/input/WDL/test_inputs-$workflow_name-local.json"
+	input_json="test_inputs-$workflow_name-local.json"
 	echo Looking at workflow $workflow_name
 	echo dir is
 	pwd
 	if [ -f $input_json ]; then
-		input_json2="inputs-$workflow_name-local.json"
-		sed s/METADATAPATH/${VIRAL_NGS_METADATA_PATH}/ > $input_json2
-		echo PASSING INPUT:
-		cat $input_json2
+		echo ${VIRAL_NGS_METADATA_PATH} > metadata_path.txt
 		#export VIRAL_NGS_METADATA_VALUE_wdl_workflow=$workflow_name
 		date
 		echo "Executing $workflow_name using Cromwell on local instance"
 		# the "cat" is to allow a pipe failure (otherwise it halts because of set -e)
 		java -jar cromwell.jar run \
 			$workflow_name.wdl \
-			-i $input_json2 | tee cromwell.out
+			-i $input_json | tee cromwell.out
 		if [ ${PIPESTATUS[0]} -gt 0 ]; then
 			echo "error running $workflow_name"
 			error_logs=$(grep stderr cromwell.out | perl -lape 's/.*\s(\S+)$/$1/g')
