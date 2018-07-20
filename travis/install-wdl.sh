36ab97eadf75773e308bf24632f97a3ee3b82df6
Danny Park <dpark@broadinstitute.org>
2018-07-20 08:30:30 -0400

    update dxWDL to 0.71 and cromwell to 33.1

--- travis/install-wdl.sh
+++ travis/install-wdl.sh
@@ -17,9 +17,9 @@ cached_fetch_jar_from_github () {
 	ln -s $CACHE_DIR/$_jar_fname $_tool_name.jar
 }
 
-cached_fetch_jar_from_github broadinstitute cromwell womtool 32
-cached_fetch_jar_from_github broadinstitute cromwell cromwell 32
-cached_fetch_jar_from_github dnanexus dxWDL dxWDL 0.69
+cached_fetch_jar_from_github broadinstitute cromwell womtool 33.1
+cached_fetch_jar_from_github broadinstitute cromwell cromwell 33.1
+cached_fetch_jar_from_github dnanexus dxWDL dxWDL 0.71
 
 TGZ=dx-toolkit-v0.255.0-ubuntu-14.04-amd64.tar.gz
 if [ ! -f $CACHE_DIR/$TGZ ]; then
