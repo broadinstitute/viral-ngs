d2cd2047bce5dbac9932b759e96db8b7de312b7e
Ilya Shlyakhter <ilya@broadinstitute.org>
2018-07-24 16:17:12 -0400

    disable ncbi tests again

--- test/integration/test_ncbi.py
+++ test/integration/test_ncbi.py
@@ -20,7 +20,7 @@ import util.file
 log = logging.getLogger(__name__)
 
 
-skip_test = False
+skip_test = True
 
 @unittest.skipIf(skip_test, "test is marked to be skipped")
 class TestNcbiFetch(TestCaseWithTmp):
