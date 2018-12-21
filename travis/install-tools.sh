58b8a6f81eee80ce06733fc69d6008a78f2af182
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-12-21 08:24:30 -0500

    updated conda channel order to reflect current bioconda guidelines.  updated requirements-minimal.txt packages to match requirements-conda.txt

--- travis/install-tools.sh
+++ travis/install-tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 
+CONDA_CHANNEL_STRING="--override-channels -c broad-viral -c conda-forge -c bioconda -c defaults"
 CONDA_ENV="$(pwd)/tools/conda-tools/default"
 
 # Set to conda's java
@@ -15,8 +16,8 @@ if [ ! -d $CONDA_ENV ]; then
 	conda create -y -m --quiet -p $CONDA_ENV python="$TRAVIS_PYTHON_VERSION"
 fi
 
-conda install -y --quiet --override-channels \
-	-c broad-viral -c r -c bioconda -c conda-forge -c defaults \
+conda install -y --quiet \
+	$CONDA_CHANNEL_STRING \
 	--file requirements-conda.txt \
 	--file requirements-conda-tests.txt \
 	--file requirements-py$PYVER.txt \
