a50e37351d4daa178504fde3efcbb41d781c0db6
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2019-02-08 15:10:39 -0500

    retry conda environment install on Travis
    installation of conda packages on Travis often fails due to transient web request errors. This wraps the install command in a retry block to attempt the install up to 3 times.

--- travis/install-tools.sh
+++ travis/install-tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e -o pipefail
+#set -e -o pipefail
 
 CONDA_CHANNEL_STRING="--override-channels -c broad-viral -c conda-forge -c bioconda -c defaults"
 CONDA_ENV="$(pwd)/tools/conda-tools/default"
@@ -13,15 +13,23 @@ export CONDA_ENVS_PATH=tools/conda-cache:tools/conda-tools/default
 PYVER=`echo $TRAVIS_PYTHON_VERSION | cut -c 1`
 
 if [ ! -d $CONDA_ENV ]; then
-	conda create -y -m --quiet -p $CONDA_ENV python="$TRAVIS_PYTHON_VERSION"
+    conda create -y -m --quiet -p $CONDA_ENV python="$TRAVIS_PYTHON_VERSION" || exit $?
 fi
 
-conda install -y --quiet \
-	$CONDA_CHANNEL_STRING \
-	--file requirements-conda.txt \
-	--file requirements-conda-tests.txt \
-	--file requirements-py$PYVER.txt \
-	-p $CONDA_ENV
+RETRIES=0
+RETRY_LIMIT=3
+until conda install -y --quiet \
+    $CONDA_CHANNEL_STRING \
+    --file requirements-conda.txt \
+    --file requirements-conda-tests.txt \
+    --file requirements-py$PYVER.txt \
+    -p $CONDA_ENV; do
+        let RETRIES++
+        if [ "$RETRIES" -gt "$RETRY_LIMIT" ]; then
+            break
+        fi
+        echo "Conda install failed, likely due to transient server errors. Retrying (${RETRIES}/${RETRY_LIMIT})..."
+done
 
 conda clean --all --yes # clean temp/cache files to reduce Travis cache size
 
