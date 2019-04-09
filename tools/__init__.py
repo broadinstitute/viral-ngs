ed8ca30e2a3bd281f69f30b29e1cbfe44ff1e757
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2019-04-08 22:24:07 -0400

    debug

--- tools/__init__.py
+++ tools/__init__.py
@@ -242,7 +242,7 @@ class CondaPackage(InstallMethod):
                 data = json.loads(self._string_from_start_of_json(command_output))
                 return data
             except Exception as e:
-                _log.debug("exception",str(e))
+                _log.warning("exception",str(e))
                 _log.warning("Failed to decode JSON during conda command '%s' emitting output: %s", " ".join(run_cmd),result.stdout.decode("UTF-8"))
                 return  # return rather than raise so we can fall back to the next install method
 
