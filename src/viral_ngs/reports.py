0cc2d16924a59c85fe8e41315c37298bed8bfdc0
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2019-06-04 13:24:24 -0400

    sort spike-ins listed in aggregated report
    sort spike-ins listed in aggregated report; also metavar positional args to avoid camel case in signature of called function. path used as base for glob is now passed through realpath to standardize the path a bit more and allow paths specified with or without trailing slashes (realpath removes them if present)

--- src/viral_ngs/reports.py
+++ src/viral_ngs/reports.py
@@ -427,11 +427,11 @@ def get_earliest_date(inDir):
     return time.strftime("%Y-%m-%d", time.localtime(earliest))
 
 
-def consolidate_spike_count(inDir, outFile):
+def consolidate_spike_count(in_dir, out_file):
     '''Consolidate multiple spike count reports into one.'''
-    with open(outFile, 'wt') as outf:
-        for fn in os.listdir(inDir):
-            fn = os.path.join(inDir, fn)
+    with open(out_file, 'wt') as outf:
+        for fn in os.listdir(in_dir):
+            fn = os.path.join(in_dir, fn)
             s = os.path.basename(fn)
             if not s.endswith('.spike_count.txt'):
                 raise Exception()
@@ -444,8 +444,8 @@ def consolidate_spike_count(inDir, outFile):
 
 
 def parser_consolidate_spike_count(parser=argparse.ArgumentParser()):
-    parser.add_argument('inDir', help='Input spike count directory.')
-    parser.add_argument('outFile', help='Output report file.')
+    parser.add_argument('in_dir', metavar="inDir", help='Input spike count directory.')
+    parser.add_argument('out_file', metavar="outFile", help='Output report file.')
     util.cmd.attach_main(parser, consolidate_spike_count, split_args=True)
     return parser
 
@@ -453,13 +453,13 @@ def parser_consolidate_spike_count(parser=argparse.ArgumentParser()):
 __commands__.append(('consolidate_spike_count', parser_consolidate_spike_count))
 
 
-def aggregate_spike_count(inDir, outFile):
+def aggregate_spike_count(in_dir, out_file):
     '''aggregate multiple spike count reports into one.'''
     spike_in_sample_counts = defaultdict(dict) # For a given spikein ID, map to sample name and corresponding count
     samples_seen = []
-    with open(outFile, 'wt') as outf:
-        for fn in glob.glob(inDir+"*.spike_count.txt"):# os.listdir():
-            #fn = os.path.join(inDir, fn)
+    with open(out_file, 'wt') as outf:
+        for fn in glob.glob(os.path.realpath(in_dir)+"/*.spike_count.txt"):# os.listdir():
+            #fn = os.path.join(in_dir, fn)
             s = os.path.basename(fn)
             if not s.endswith('.spike_count.txt'):
                 raise Exception()
@@ -476,7 +476,7 @@ def aggregate_spike_count(inDir, outFile):
                         spike_in_sample_counts[spike][s] = count
                         #outf.write('\t'.join([s, spike, count]) + '\n')
         outf.write("\t".join(["spike-in"]+samples_seen)+"\n")
-        for spike in spike_in_sample_counts.keys():
+        for spike in sorted(spike_in_sample_counts.keys()):
             row = []
             row.append(spike)
             for s in samples_seen:
@@ -488,8 +488,8 @@ def aggregate_spike_count(inDir, outFile):
 
 
 def parser_aggregate_spike_count(parser=argparse.ArgumentParser()):
-    parser.add_argument('inDir', help='Input spike count directory.')
-    parser.add_argument('outFile', help='Output report file.')
+    parser.add_argument('in_dir', metavar="inDir", help='Input spike count directory.')
+    parser.add_argument('out_file', metavar="outFile", help='Output report file.')
     util.cmd.attach_main(parser, aggregate_spike_count, split_args=True)
     return parser
 
