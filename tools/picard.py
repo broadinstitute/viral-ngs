42d420d3c8f065295c8a6a9120e56e5b6b2b0b9e
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2019-08-09 16:58:39 -0400

    roll back test input to state of origin/master; add functions to util.file to sanitize fasta file IDs
    fasta ID is sanitized for picard CreateSequenceDictionary to adhere to character set restrictions in SAM/BAM RNAME spec. see: https://github.com/samtools/hts-specs/pull/333
    
    Roll back test input now that we are doing the sanitization. As of this commit, sanitization is only performed on fasta files destined for CreateSequenceDictionary

--- tools/picard.py
+++ tools/picard.py
@@ -19,7 +19,7 @@ import util.file
 import util.misc
 
 TOOL_NAME = "picard"
-TOOL_VERSION = '2.20.3'
+TOOL_VERSION = '2.20.5'
 TOOL_URL = 'https://github.com/broadinstitute/picard/releases/download/' \
     + '{ver}/picard-tools-{ver}.zip'.format(ver=TOOL_VERSION)
 # Note: /seq/software/picard/{versionnumber}/ does not correspond with github release numbers!
@@ -408,8 +408,9 @@ class CreateSequenceDictionaryTool(PicardTools):
                 os.unlink(outDict)
             else:
                 return
-        opts = ['REFERENCE=' + inFasta, 'OUTPUT=' + outDict]
-        PicardTools.execute(self, self.subtoolName, opts + picardOptions, JVMmemory)
+        with util.file.fastas_with_sanitized_ids(inFasta, use_tmp=False) as sanitized_fastas:
+            opts = ['REFERENCE=' + sanitized_fastas[0], 'OUTPUT=' + outDict]
+            PicardTools.execute(self, self.subtoolName, opts + picardOptions, JVMmemory)
 
 
 class BuildBamIndexTool(PicardTools):
