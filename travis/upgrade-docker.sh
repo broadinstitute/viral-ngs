d4b32b8d935b450328dbac456fe7ade2de8e3262
Danny Park <dpark@broadinstitute.org>
2019-09-09 23:52:09 -0400

    update passwords

--- travis/upgrade-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# upgrade docker (Travis's is old)
-#sudo apt-get update || echo "apt-get update failed, but we will ignore that"
-sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
-docker --version
