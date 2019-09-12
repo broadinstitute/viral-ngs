4a2bf848796226868e4b866af873292a0fe9b9dc
Danny Park <dpark@broadinstitute.org>
2019-09-12 16:00:51 -0400

    remove all test files from resulting docker image, mount the test code & input data in at runtime instead. load gatk3 at docker build time.

---
+++ docker/install-gatk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e -o pipefail
+
+GATK_VERSION=3.8
+GATK_BUILD=1-0-gf15c1c3ef
+GATK_URL="https://software.broadinstitute.org/gatk/download/auth?package=GATK-archive&version=${GATK_VERSION}-${GATK_BUILD}"
+GATK_TAR=GenomeAnalysisTK-${GATK_VERSION}.tar
+GATK_JAR=GenomeAnalysisTK.jar
+GATK_JAR_MD5=186aee868bb7cffc18007966ace8d053
+
+echo "Fetching GATK bundle"
+pwd
+wget --quiet "${GATK_URL}" -O ${GATK_TAR}
+
+tar --warning=no-unknown-keyword -xvpf ${GATK_TAR}
+rm ${GATK_TAR}
+mv GenomeAnalysisTK-${GATK_VERSION}-${GATK_BUILD}/GenomeAnalysisTK.jar ${GATK_JAR}
+rmdir GenomeAnalysisTK-${GATK_VERSION}-${GATK_BUILD}
+
+md5sum --strict -c <(echo ${GATK_JAR_MD5} ${GATK_JAR})
+gatk3-register ${GATK_JAR}
+rm ${GATK_JAR}
--- run_all_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e -x -o pipefail
-
-pytest test/unit test/integration
