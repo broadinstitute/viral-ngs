d651b9e36e7047a470c56ed82301148cba420489
Danny Park <dpark@broadinstitute.org>
2019-10-16 13:04:37 -0400

    upgrade dx-toolkit 0.285 to 0.288

--- travis/install-wdl.sh
+++ travis/install-wdl.sh
@@ -21,7 +21,7 @@ cached_fetch_jar_from_github broadinstitute cromwell womtool 47
 cached_fetch_jar_from_github broadinstitute cromwell cromwell 47
 cached_fetch_jar_from_github dnanexus dxWDL dxWDL v1.33
 
-TGZ=dx-toolkit-v0.285.0-ubuntu-16.04-amd64.tar.gz
+TGZ=dx-toolkit-v0.288.0-ubuntu-16.04-amd64.tar.gz
 if [ ! -f $CACHE_DIR/$TGZ ]; then
 	echo "Fetching $TGZ"
 	wget --quiet https://dnanexus-sdk.s3.amazonaws.com/$TGZ
