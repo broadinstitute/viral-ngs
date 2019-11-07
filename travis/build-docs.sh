fcc9a98f467d28940bd3eecc27134e0ee40e7545
Danny Park <dpark@broadinstitute.org>
2019-11-07 10:08:04 -0500

    add travis canary job for docs

--- travis/build-docs.sh
+++ travis/build-docs.sh
@@ -2,8 +2,6 @@
 
 set -e -o pipefail
 
-if [[ ( -n $TRAVIS_PULL_REQUEST && $TRAVIS_PULL_REQUEST != "false" ) || $TRAVIS_BRANCH = "master" || -n "$TRAVIS_TAG" ]]; then
-    pushd docs
-    make html && echo "Docs built successfully!" || echo "Docs did NOT build successfully."
-    popd
-fi
+pushd docs
+make html && echo "Docs built successfully!" || echo "Docs did NOT build successfully."
+popd
