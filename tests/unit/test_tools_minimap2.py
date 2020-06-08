85883e3c097aeddeeaae491ab2ecc96ce4a88478
Danny Park <dpark@broadinstitute.org>
2020-06-08 15:15:52 -0400

    update tests

--- tests/unit/test_tools_minimap2.py
+++ tests/unit/test_tools_minimap2.py
@@ -22,13 +22,24 @@ class TestToolMinimap2(TestCaseWithTmp):
         root_input_dir = util.file.get_test_input_path()
         self.ref_fasta = os.path.join(root_input_dir, '5kb_human_from_chr6.fasta')
 
-    def test_working_bam(self):
+    def test_human_bam(self):
         in_bam = os.path.join(util.file.get_test_input_path(), "TestDepleteHuman", 'test-reads-human.bam')
 
         outfile = util.file.mkstempfname('.bam')
-        self.mm2.align_bam(in_bam, self.ref_fasta, outfile)
+        self.mm2.align_bam(in_bam, self.ref_fasta, outfile, options=['-x', 'sr'])
 
         self.assertEqual(self.samtools.count(outfile), 20)
+
+        os.unlink(outfile)
+
+    def test_ebola_bam(self):
+        in_bam = os.path.join(util.file.get_test_input_path(), 'G5012.3.testreads.bam')
+        ref_fasta = os.path.join(util.file.get_test_input_path(), 'ebov-makona.fasta')
+
+        outfile = util.file.mkstempfname('.bam')
+        self.mm2.align_bam(in_bam, ref_fasta, outfile)
+
+        self.assertEqual(self.samtools.count(outfile), 18843)
 
         os.unlink(outfile)
 
