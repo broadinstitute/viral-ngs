70f5080bb7ec073cfa60de732103c6b131d21572
Danny Park <dpark@broadinstitute.org>
2020-06-09 16:24:16 -0400

    tighten up test code and switch to util.file.tmpfname context manager

--- tests/unit/test_tools_minimap2.py
+++ tests/unit/test_tools_minimap2.py
@@ -15,40 +15,26 @@ class TestToolMinimap2(TestCaseWithTmp):
     def setUp(self):
         super(TestToolMinimap2, self).setUp()
         self.mm2 = tools.minimap2.Minimap2()
-        self.mm2.install()
         self.samtools = tools.samtools.SamtoolsTool()
-
         self.tempDir = tempfile.mkdtemp()
         root_input_dir = util.file.get_test_input_path()
         self.ref_fasta = os.path.join(root_input_dir, '5kb_human_from_chr6.fasta')
 
     def test_human_bam(self):
         in_bam = os.path.join(util.file.get_test_input_path(), "TestDepleteHuman", 'test-reads-human.bam')
-
-        outfile = util.file.mkstempfname('.bam')
-        self.mm2.align_bam(in_bam, self.ref_fasta, outfile, options=['-x', 'sr'])
-
-        self.assertEqual(self.samtools.count(outfile), 20)
-
-        os.unlink(outfile)
+        with util.file.tmpfname('.bam') as outfile:
+            self.mm2.align_bam(in_bam, self.ref_fasta, outfile, options=['-x', 'sr'])
+            self.assertEqual(self.samtools.count(outfile), 20)
 
     def test_ebola_bam(self):
         in_bam = os.path.join(util.file.get_test_input_path(), 'G5012.3.testreads.bam')
         ref_fasta = os.path.join(util.file.get_test_input_path(), 'ebov-makona.fasta')
-
-        outfile = util.file.mkstempfname('.bam')
-        self.mm2.align_bam(in_bam, ref_fasta, outfile)
-
-        self.assertEqual(self.samtools.count(outfile), 18843)
-
-        os.unlink(outfile)
+        with util.file.tmpfname('.bam') as outfile:
+            self.mm2.align_bam(in_bam, ref_fasta, outfile)
+            self.assertEqual(self.samtools.count(outfile), 18843)
 
     def test_corrupt_bam(self):
-        in_bam = os.path.join(util.file.get_test_input_path(), 'broken.bam')
-
-        outfile = util.file.mkstempfname('.bam')
-
         # pipe.poll() should raise an exception
-        self.assertRaises(subprocess.CalledProcessError, self.mm2.align_bam, in_bam, self.ref_fasta, outfile)
-
-        os.unlink(outfile)
+        in_bam = os.path.join(util.file.get_test_input_path(), 'broken.bam')
+        with util.file.tmpfname('.bam') as outfile:
+            self.assertRaises(subprocess.CalledProcessError, self.mm2.align_bam, in_bam, self.ref_fasta, outfile)
