e97dc89883b6ee46b788230ee5f2e86a1cb37fdc
Danny Park <dpark@broadinstitute.org>
2024-02-07 13:27:22 -0500

    attempt to fix rtd build

--- src/viral_ngs/metagenomics.py
+++ src/viral_ngs/metagenomics.py
@@ -1404,6 +1404,263 @@ def taxlevel_summary(summary_files_in, json_out, csv_out, tax_headings, taxlevel
 __commands__.append(('taxlevel_summary', parser_kraken_taxlevel_summary))
 
 
+def parser_kraken_taxlevel_plurality(parser=argparse.ArgumentParser()):
+    parser.add_argument('summary_file', help='input Kraken-format summary text file with tab-delimited taxonomic levels.')
+    parser.add_argument('tax_heading', help='The taxonomic heading to analyze.')
+    parser.add_argument('out_report', help='tab-delimited output file.')
+    util.cmd.common_args(parser, (('loglevel', None), ('version', None), ('tmp_dir', None)))
+    util.cmd.attach_main(parser, taxlevel_summary, split_args=True)
+    return parser
+
+def taxlevel_plurality(summary_file, tax_heading, out_report):
+    """
+        Identifies the most abundant taxon (of any rank) contributing to a node of interest in the taxonomic tree.
+        It is intended to highlight the primary contributor of taxonomic signal within a taxonomic category of interest,
+        for example, the most abundant virus among all viruses.
+    """
+
+    samples = {}
+    same_level = False
+
+    Abundance = collections.namedtuple("Abundance", "percent,count,kmers,dup,cov")
+
+    def indent_len(in_string):
+        return len(in_string)-len(in_string.lstrip())
+
+    for f in list(summary_files_in):
+        sample_name, extension = os.path.splitext(f)
+        sample_summary = {}
+        sample_root_summary = {}
+        tax_headings_copy = [s.lower() for s in tax_headings]
+
+        # -----------------------------------------------------------------
+        # KrakenUniq has two lines prefixed by '#', a blank line,
+        # and then a TSV header beginning with "%". The column fields are:
+        # (NB:field names accurate, but space-separated in this comment 
+        # for readability here)
+        #   %        reads  taxReads  kmers  dup   cov  taxID  rank          taxName
+        #   0.05591  2      0         13     1.85  NA   10239  superkingdom  Viruses
+        #
+        # Where the fields are:
+        #   %:
+        #   reads:
+        #   taxReads:
+        #   kmers: number of unique k-mers
+        #   dup: average number of times each unique k-mer has been seen
+        #   cov: coverage of the k-mers of the clade in the database
+        #   taxID: 
+        #   rank: row["rank"]; A rank code (see list below)
+        #   taxName: row["sci_name"]; indented scientific name
+        #
+        # Taxonomic ranks used by KrakenUniq include:
+        #   unknown, no rank, sequence, assembly, subspecies, 
+        #   species, species subgroup, species group, subgenus, 
+        #   genus, tribe, subfamily, family, superfamily, parvorder, 
+        #   infraorder, suborder, order, superorder, parvclass, 
+        #   infraclass, subclass, class, superclass, subphylum, 
+        #   phylum, kingdom, superkingdom, root
+        #
+        #   via: https://github.com/fbreitwieser/krakenuniq/blob/a8b4a2dbf50553e02d3cab3c32f93f91958aa575/src/taxdb.hpp#L96-L131
+        # -----------------------------------------------------------------
+        # Kraken (standard) reports lack header lines. 
+        # (NB:field names below are only for reference. Space-separated for 
+        # readability here)
+        #   %     reads  taxReads  rank  taxID      taxName
+        #   0.00  16     0         D     10239      Viruses
+        #
+        # Where the fields are:
+        #   %:        row["pct_of_reads"]; Percentage of reads covered by the clade rooted at this taxon
+        #   reads:    row["num_reads"]; Number of reads covered by the clade rooted at this taxon
+        #   taxReads: row["reads_exc_children"]; Number of reads assigned directly to this taxon
+        #   rank:     row["rank"]; A rank code, indicating (U)nclassified, (D)omain, (K)ingdom, (P)hylum, (C)lass, (O)rder, (F)amily, (G)enus, or (S)pecies. All other ranks are simply '-'.
+        #   taxID:    row["NCBI_tax_ID"]; NCBI taxonomy ID
+        #   taxName:  row["sci_name"]; indented scientific name
+        # -----------------------------------------------------------------
+
+
+        with util.file.open_or_gzopen(f, 'rt') as inf:
+            report_type=None
+            should_process = False
+            indent_of_selection = -1
+            currently_being_processed = ""
+            for lineno, line in enumerate(inf):
+                if len(line.rstrip('\r\n').strip()) == 0 or ( report_type != None and line.startswith("#") or line.startswith("%")):
+                    continue
+
+                # KrakenUniq is mentioned on the first line of
+                # summary reports created by KrakenUniq
+                if not report_type and "KrakenUniq" in line:
+                    report_type="krakenuniq"
+                    continue
+                elif not report_type:
+                    report_type="kraken"                
+
+                csv.register_dialect('kraken_report', quoting=csv.QUOTE_MINIMAL, delimiter="\t")
+                if report_type == "kraken":
+                    fieldnames = [ "pct_of_reads",
+                                    "num_reads",
+                                    "reads_exc_children",
+                                    "rank",
+                                    "NCBI_tax_ID",
+                                    "sci_name"
+                                ]
+                elif report_type == "krakenuniq":
+                    fieldnames = [ 
+                                    "pct_of_reads",
+                                    "num_reads",
+                                    "reads_exc_children",
+                                    "uniq_kmers",
+                                    "kmer_dups",
+                                    "cov_of_clade_kmers",
+                                    "NCBI_tax_ID",
+                                    "rank",
+                                    "sci_name"
+                                ]
+                else:
+                    continue #never reached since we fall back to kraken above
+
+                row = next(csv.DictReader([line.strip().rstrip('\n')], fieldnames=fieldnames, dialect="kraken_report"))
+
+                try:
+                    indent_of_line = indent_len(row["sci_name"])
+                except AttributeError as e:
+                    log.warning("Report type: '{}'".format(report_type))
+                    log.warning("Issue with line {}: '{}'".format(lineno,line.strip().rstrip('\n')))
+                    log.warning("From file: {}".format(f))
+                # remove leading/trailing whitespace from each item
+                row = { k:v.strip() for k, v in row.items()}
+
+                # rows are formatted as described above. 
+                # Kraken:
+                #   0.00  16  0   D   10239     Viruses
+                # KrakenUniq:
+                #   0.05591  2      0         13     1.85  NA   10239  superkingdom  Viruses
+
+                # if the root-level bins (root, unclassified) should be included, do so, but bypass normal
+                # stateful parsing logic since root does not have a distinct rank level
+                if row["sci_name"].lower() in ["root","unclassified"] and include_root:
+                    sample_root_summary[row["sci_name"]] = collections.OrderedDict()
+                    sample_root_summary[row["sci_name"]][row["sci_name"]] = Abundance(float(row["pct_of_reads"]), int(row["num_reads"]),row.get("kmers",None),row.get("dup",None),row.get("cov",None))
+                    continue
+
+                if indent_of_line <= indent_of_selection:
+                    should_process = False
+                    indent_of_selection=-1
+
+                if indent_of_selection == -1:
+                    if row["sci_name"].lower() in tax_headings_copy:
+                        tax_headings_copy.remove(row["sci_name"].lower())
+
+                        should_process = True
+                        indent_of_selection = indent_of_line
+                        currently_being_processed = row["sci_name"]
+                        sample_summary[currently_being_processed] = collections.OrderedDict()
+                        if row["rank"] == rank_code(taxlevel_focus) or row["rank"].lower().replace(" ","") == taxlevel_focus.lower().replace(" ",""):
+                            same_level = True
+                        if row["rank"] in ("-","no rank"):
+                            log.warning("Non-taxonomic parent level selected")
+
+                if should_process:
+                    # skip "-" rank levels since they do not occur at the sample level
+                    # otherwise include the taxon row if the rank matches the desired level of focus
+                    if (row["rank"] not in ("-","no rank") and (rank_code(taxlevel_focus) == row["rank"] or row["rank"].lower().replace(" ","") == taxlevel_focus.lower().replace(" ","")) ):
+                        if int(row["num_reads"])>=count_threshold:
+                            sample_summary[currently_being_processed][row["sci_name"]] = Abundance(float(row["pct_of_reads"]), int(row["num_reads"]),row.get("kmers",None),row.get("dup",None),row.get("cov",None))
+
+
+        for k,taxa in sample_summary.items():
+            sample_summary[k] = collections.OrderedDict(sorted(taxa.items(), key=lambda item: (item[1][1]) , reverse=True)[:top_n_entries])
+
+            if len(list(sample_summary[k].items()))>0:
+                log.info("{f}: most abundant among {heading} at the {level} level: "
+                            "\"{name}\" with {reads} reads ({percent:.2%} of total); "
+                            "included since >{threshold} read{plural}".format(
+                                                                          f=f,
+                                                                          heading=k,
+                                                                          level=taxlevel_focus,
+                                                                          name=list(sample_summary[k].items())[0][0],
+                                                                          reads=list(sample_summary[k].items())[0][1].count,
+                                                                          percent=list(sample_summary[k].items())[0][1].percent/100.0,
+                                                                          threshold=count_threshold,
+                                                                          plural="s" if count_threshold>1 else "" )
+                )
+
+        if include_root:
+            # include root-level bins (root, unclassified) in the returned data
+            for k,taxa in sample_root_summary.items():
+                assert (k not in sample_summary), "{k} already in sample summary".format(k=k)
+                sample_summary[k] = taxa
+        samples[sample_name] = sample_summary
+
+    if json_out != None:
+        json_summary = json.dumps(samples, sort_keys=True, indent=4, separators=(',', ': '))
+        json_out.write(json_summary)
+        json_out.close()
+
+
+    if csv_out != None:
+
+        # if we're writing out at the same level as the query header
+        # write out the fractions and counts
+        if same_level or no_hist:
+
+            fieldnames = set()
+            for sample, taxa in samples.items():
+                for heading,taxon in taxa.items():
+                    if len(taxon):
+                        for k in taxon.keys():
+                            fieldnames |= set([k+"-pt",k+"-ct"])
+
+            heading_columns = ["sample"]
+            if include_root:
+                root_fields = ["root-pt","root-ct","unclassified-pt","unclassified-ct"]
+                fieldnames -= set(root_fields)
+                heading_columns += root_fields
+
+            writer = csv.DictWriter(csv_out, restval=0 if zero_fill else '', fieldnames=heading_columns+sorted(list(fieldnames)))
+            writer.writeheader()
+
+            for sample, taxa in samples.items():
+                sample_dict = {}
+                sample_dict["sample"] = sample
+                for heading,taxon in taxa.items():
+                    for entry in taxon.keys():
+                        sample_dict[entry+"-pt"] = taxon[entry].percent
+                        sample_dict[entry+"-ct"] = taxon[entry].count
+                writer.writerow(sample_dict)
+
+
+            csv_out.close()
+
+        # otherwise write out a histogram
+        else:
+            count = 0
+            summary_counts = collections.defaultdict(dict)
+            for sample, totals in samples.items():
+                for heading,taxa in totals.items():
+                    for taxon in taxa.keys():
+                        if taxon not in summary_counts[heading].keys():
+                            summary_counts[heading][taxon] = 1
+                        else:
+                            summary_counts[heading][taxon] += 1
+
+            for k,taxa in summary_counts.items():
+                summary_counts[k] = collections.OrderedDict(sorted(taxa.items(), key=lambda item: (item[1]) , reverse=True))
+
+
+            fieldnames = ["heading","taxon","num_samples"]
+            writer = csv.DictWriter(csv_out, restval=0 if zero_fill else '', fieldnames=fieldnames)
+            writer.writeheader()
+
+            for heading,taxa_counts in summary_counts.items():
+                writer.writerows([{"heading":heading,"taxon":taxon,"num_samples":count} for taxon,count in taxa_counts.items()])
+
+            csv_out.close()
+
+
+__commands__.append(('taxlevel_plurality', parser_kraken_taxlevel_plurality))
+
+
 def parser_krona_build(parser=argparse.ArgumentParser()):
     parser.add_argument('db', help='Krona taxonomy database output directory.')
     parser.add_argument('--taxdump_tar_gz', help='NCBI taxdump.tar.gz file', default=None)
